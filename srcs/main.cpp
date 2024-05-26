9522472a889061bf835365736dfbc49261abc928
habvi <93956895+habvi@users.noreply.github.com>
2024-05-26 19:37:21 +0900

    [update] main / configをparseしてserverに渡してRun()の流れのベース

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,14 +1,28 @@
+#include "Config.hpp"
+#include "Server.hpp"
 #include <cstdlib> // EXIT_
 #include <iostream>
+#include <string>
 
 // ./webserv [configuration file]
 int main(int argc, char **argv) {
-    // if (argc != 2) {
-    //     std::cerr <<"Error: invalid arguments" << std::endl; 
-    //     return EXIT_FAILURE;
-    // }
-    (void)argc;
-    (void)argv;
-    std::cout << "webserv(kari)" << std::endl;
-    return EXIT_SUCCESS;
+	if (argc > 2) {
+		std::cerr << "Error: invalid arguments" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// todo: handle parse error
+	std::string path_config;
+	if (argc == 1) {
+		path_config = "default.conf";
+	} else {
+		path_config = std::string(argv[1]);
+	}
+	Config::MapConfig config = Config::ParseConfig(path_config);
+
+	// todo: try-catch?
+	Server server(config);
+	server.Run();
+
+	return EXIT_SUCCESS;
 }
