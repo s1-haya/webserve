3b38ebaef166e4551730d9932571fdd0174c9870
Eito Rin <erin@c1r7s2.42tokyo.jp>
2024-05-26 20:04:11 +0900

    [add] ConfigParseクラスにServerDirを追加
    追記: ConfigParse::GetInstance() (ConfiguInstanceエイリアス)でインスタンスを取り出して使用
    ServerDir変数をpublicにすることでConfigInstance->server_のようにアクセス可能(constにしていきたい)
    インスタンスが一つしかないとすればこれで良い？
    ASTを利用したパース機能を追加していきたい

--- ConfigParse/config_parse.hpp
+++ ConfigParse/config_parse.hpp
@@ -1,25 +1,28 @@
 #ifndef CONFIG_PARSE_HPP_
-# define CONFIG_PARSE_HPP_
+#define CONFIG_PARSE_HPP_
 
-# include <iostream>
-# include "server.hpp"
+#include "server.hpp"
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
-# define ConfigInstance ConfigParse::GetInstance()
+#define ConfigInstance ConfigParse::GetInstance()
 
-class ConfigParse
-{
-private:
-	ConfigParse(std::string& file_path);
-	ConfigParse(const ConfigParse&);
-	ConfigParse& operator=(const ConfigParse&);
-	static const ConfigParse* s_cInstance;
+class ConfigParse {
+  private:
+	ConfigParse(std::string &file_path);
+	ConfigParse(const ConfigParse &);
+	ConfigParse              &operator=(const ConfigParse &);
+	static const ConfigParse *s_cInstance;
 
-public:
+  public:
 	~ConfigParse();
 
-	static const ConfigParse* GetInstance();
-	static void Create(std::string& file_path);
-	static void Destroy();
+	static const ConfigParse *GetInstance();
+	static void               Create(std::string &file_path);
+	static void               Destroy();
+	ServerDir                 server_; // getterなしのこの構造で良い?
 };
 
 #endif
--- ConfigParse/main.cpp
+++ ConfigParse/main.cpp
@@ -5,4 +5,5 @@
 int main() {
 	std::string conf = "config";
 	ConfigInstance->Create(conf);
+	std::cout << ConfigInstance->server_.server_name_ << std::endl;
 }
