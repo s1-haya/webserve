2034fb4c134a2043084211ab64a2b77b1eaf2538
s1-haya <sunzecun@gmail.com>
2024-05-26 17:05:05 +0900

    create the readFile funtion
    name: readFile
    type: const std::string
    return:  the contents of a file if the file exsits. If the file doesn't exist, the contents of 404 error page return.

--- sandbox/sawa/http/fileExists.cpp
+++ sandbox/sawa/http/fileExists.cpp
@@ -6,18 +6,18 @@ bool fileExists(const std::string& filePath) {
     return (file.good());
 }
 
-int main() {
-    const std::string& path = "./example.txt";
-    if (fileExists(path)) {
-        std::cout << "File exists." << std::endl;
-    } else {
-        std::cout << "File does not exist." << std::endl;
-    }
-    const std::string& notAuthorizedFilePath = "notAuthorizedFile";
-    if (fileExists(notAuthorizedFilePath)) {
-        std::cout << "File exists." << std::endl;
-    } else {
-        std::cout << "File does not exist." << std::endl;
-    }
-    return (0);
-}
+// int main() {
+//     const std::string& path = "./example.txt";
+//     if (fileExists(path)) {
+//         std::cout << "File exists." << std::endl;
+//     } else {
+//         std::cout << "File does not exist." << std::endl;
+//     }
+//     const std::string& notAuthorizedFilePath = "notAuthorizedFile";
+//     if (fileExists(notAuthorizedFilePath)) {
+//         std::cout << "File exists." << std::endl;
+//     } else {
+//         std::cout << "File does not exist." << std::endl;
+//     }
+//     return (0);
+// }
