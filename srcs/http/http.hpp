44dc6941fea62ecd0859326b0db07ec385112333
habvi <93956895+habvi@users.noreply.github.com>
2024-06-01 03:41:19 +0900

    [add] http / http classで使うmapと仮に必要そうな型を定義
    mapをメンバ変数にしたのはparse後にその値を使って更にrequest_が増え、その後request_を使ってresponseを作成するのでこのスコープが良いかと。
    MessageTypeも共通して使うのでenumで定義したが、もっと良い方法があるかも

--- srcs/http/http.hpp
+++ srcs/http/http.hpp
@@ -2,10 +2,18 @@
 #define HTTP_HPP_
 
 #include "debug.hpp" // todo: tmp
+#include <map>
 #include <string>
 
 class Http {
   public:
+	enum MessageType {
+		HTTP_METHOD,
+		HTTP_PATH,
+		HTTP_CONTENT,
+		HTTP_STATUS
+	};
+	typedef std::map<MessageType, std::string> RequestMessage;
 	// todo: tmp
 	Http();
 	~Http();
@@ -14,6 +22,8 @@ class Http {
 	// prohibit copy
 	Http(const Http &other);
 	Http &operator=(const Http &other);
+	// variable
+	RequestMessage request_;
 };
 
 #endif /* HTTP_HPP_ */
