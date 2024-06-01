6c9f8fdd45f4dc14da59a7d984daf6f21d18c07e
s1-haya <sunzecun@gmail.com>
2024-06-01 15:42:33 +0900

    Add OutputStatusLine(RequestMessage request);
    ステータスラインはHTTPレスポンスの一行目の部分です。ステータスコードとステータステキストは紐づいてるので構造体にしてもいいかも
    
    一般的なステータスコードとステータステキスト
    200 OK
    302 Found
    404 Not Found
    
    メソッド特有のステータスコードもあるみたいだから調査が必要。例えばDELETEの場合は200ではなく204とか
    > PUT や DELETE の成功結果は 200 OK ではなく、 204 No Content (や、リソースの初回アップロードによる作成の場合は 201 Created )である場合もある。

---
+++ srcs/http/http_response_message_format.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+
+enum MessageType {
+	HTTP_METHOD,
+	HTTP_PATH,
+	HTTP_CONTENT,
+	HTTP_STATUS,
+	HTTP_STATUS_TEXT,
+};
+
+typedef std::map<MessageType, std::string> RequestMessage;
+
+void OutputStatusLine(RequestMessage request) {
+	std::cout << "HTTP/1.1"
+			  << " " << request[HTTP_STATUS] << " " << request[HTTP_STATUS_TEXT]
+			  << std::endl;
+}
+
+int main(void) {
+	RequestMessage request_;
+	request_[HTTP_METHOD]  = "GET";
+	request_[HTTP_PATH]    = "/html/index.html";
+	request_[HTTP_STATUS]  = "200";
+	request_[HTTP_STATUS_TEXT]  = "OK";
+	request_[HTTP_CONTENT] = "<!DOCTYPE html><html<body><h1>Hello "
+							 "from webserv!(tmp)</h1></body></html>";
+	OutputStatusLine(request_);
+	return 0;
+}
