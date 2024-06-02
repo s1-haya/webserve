76a65edd79b1939a65151f18b3a9ebaca6753ca0
s1-haya <sunzecun@gmail.com>
2024-06-02 10:37:13 +0900

    Update: OutputHeaderField
    OWSは省略可能な空白。空白か水平タブどちらかを0回以上って意味。
    今回レスポンスのメッセージ書式だから開発者が決めると思うのでSPにした。
         OWS            = *( SP / HTAB )
                        ; optional whitespace

--- srcs/http/output_header_field.cpp
+++ srcs/http/output_header_field.cpp
@@ -1,10 +1,12 @@
-#include "iostream"
+#include <iostream>
+#include "character.hpp"
 
 void OutputHeaderField(void) {
-	std::cout << "field-name" << ":" << "OWS" << "field-value" << "OWS";
+	std::cout << "field-name"
+			  << ":" << SP << "field-value" << SP << CR << LF;
 }
 
- int main(void) {
+int main(void) {
 	OutputHeaderField();
 	return 0;
- }
+}
