efe188de47b7c2636f3289becef17a6120db9847
habvi <93956895+habvi@users.noreply.github.com>
2024-06-04 00:46:06 +0900

    [add] Parse Request / delimiterでstringを切り取りvectorに格納して返す関数追加。1行目がstart_line

--- srcs/http/request/parse.cpp
+++ srcs/http/request/parse.cpp
@@ -1,9 +1,33 @@
 #include "http.hpp"
+#include <vector>
+
+namespace {
+	std::vector<std::string>
+	SplitStr(const std::string &str, const std::string delim) {
+		std::vector<std::string> split_str;
+
+		std::string::const_iterator it = str.begin();
+		while (it != str.end()) {
+			const std::string      head = &(*it);
+			std::string::size_type pos  = head.find(delim);
+			if (pos == std::string::npos) {
+				split_str.push_back(head.substr(0));
+				break;
+			}
+			split_str.push_back(head.substr(0, pos));
+			std::advance(it, pos);
+			++it;
+		}
+		return split_str;
+	}
+} // namespace
 
 // todo: tmp request_
 void Http::ParseRequest(const std::string &read_buf) {
-	(void)read_buf;
-	// todo: parse
+	const std::vector<std::string> lines      = SplitStr(read_buf, "\r\n");
+	const std::string              start_line = lines[0];
+	(void)start_line;
+
 	request_[HTTP_METHOD] = "GET";
 	request_[HTTP_PATH]   = "/html/index.html";
 }
