49563086ca19328f16e851fe5f4e24bb4f636378
s1-haya <sunzecun@gmail.com>
2024-06-03 22:31:52 +0900

    Refactor: utilsにあるcharacter.cpp, hppをhttp/utils内に移動
    変更理由
    - http内で使用するutilsのため
    
    確認したこと
    - コンパイルできるかどうか
    	c++ -I./srcs/http -I./srcs/http/utils -I./srcs/utils  ./srcs/http/*.cpp ./srcs/http/response/*.cpp ./srcs/http/utils/*.cpp sandbox/sawa/test/test_create_response.cpp

---
+++ srcs/http/utils/character.hpp
@@ -0,0 +1,8 @@
+#ifndef HTTP_UTILS_CHARACTER_HPP_
+#define HTTP_UTILS_CHARACTER_HPP_
+
+extern const char SP;
+extern const char CR;
+extern const char LF;
+
+#endif /* HTTP_UTILS_CHARACTER_HPP_ */
--- srcs/utils/character.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_CHARACTER_HPP_
-#define UTILS_CHARACTER_HPP_
-
-extern const char SP;
-extern const char CR;
-extern const char LF;
-
-#endif /* UTILS_CHARACTER_HPP_ */
