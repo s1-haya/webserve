d7e28a291d75716b926c57180726eff1b2a823bb
habvi <93956895+habvi@users.noreply.github.com>
2024-06-04 02:22:19 +0900

    [tmp] http / ReadFileをsandboxから持ってきて合わせたがなぜかエラー…

--- srcs/http/http.cpp
+++ srcs/http/http.cpp
@@ -1,5 +1,7 @@
 #include "http.hpp"
 #include "convert.hpp"
+#include <fstream>
+#include <sstream>
 
 Http::Http(const std::string &read_buf) {
 	ParseRequest(read_buf);
@@ -16,13 +18,30 @@ void Http::ParseRequest(const std::string &read_buf) {
 	request_[HTTP_PATH]   = "/html/index.html";
 }
 
+namespace {
+	bool FileExists(const std::string &file_path) {
+		std::ifstream file(file_path.c_str());
+		return file.good();
+	}
+
+	std::ifstream open_file(const std::string &file_path) {
+		if (!FileExists(file_path)) {
+			return std::ifstream("html/404.html");
+		}
+		return std::ifstream(file_path.c_str());
+	}
+
+	const std::string ReadFile(const std::string &file_path) {
+		std::ifstream     file = open_file(file_path);
+		std::stringstream buffer;
+		buffer << file.rdbuf();
+		return buffer.str();
+	}
+} // namespace
+
 // todo: tmp content
 void Http::ReadPathContent() {
-	const std::string path = request_[HTTP_PATH];
-	(void)path;
-	// todo: read path content
-	const std::string content = "<!DOCTYPE html><html<body><h1>Hello "
-								"from webserv!(tmp)</h1></body></html>";
+	const std::string content = ReadFile(request_[HTTP_PATH]);
 	request_[HTTP_CONTENT]    = content;
 	request_[HTTP_STATUS]     = "200";
 }
