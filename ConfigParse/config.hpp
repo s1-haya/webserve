f3313a3583facea3ab75ab833eecb0f776b8b62b
林穎涛 <eitou.l@RinEitoMacBook-Air.local>
2024-06-06 10:27:27 +0900

    [update] ConfigParseクラスをConfigクラスに名前変更、fstreamでファイルを読み取るところまで

---
+++ ConfigParse/config.hpp
@@ -0,0 +1,26 @@
+#ifndef CONFIG_PARSE_HPP_
+#define CONFIG_PARSE_HPP_
+
+#include "server.hpp"
+#include <fstream>
+
+#define ConfigInstance Config::GetInstance()
+
+class Config {
+  private:
+	Config(std::string &file_path);
+	Config(const Config &);
+	Config              &operator=(const Config &);
+	static const Config *s_cInstance;
+
+  public:
+	~Config();
+
+	static const Config *GetInstance();
+	static void          Create(std::string &file_path);
+	static void          Destroy();
+	ServerDir            server_;
+	std::ifstream        config_file_;
+};
+
+#endif
