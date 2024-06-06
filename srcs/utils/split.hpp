ec07955b6ffea71e203ae5700465ec1689359d0f
habvi <93956895+habvi@users.noreply.github.com>
2024-06-06 10:03:41 +0900

    [add] utils / 主にparseで使用するSplitStr()をutilsに追加

---
+++ srcs/utils/split.hpp
@@ -0,0 +1,9 @@
+#ifndef UTILS_SPLIT_HPP_
+#define UTILS_SPLIT_HPP_
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> SplitStr(const std::string &str, const std::string delim);
+
+#endif /* UTILS_SPLIT_HPP_ */
