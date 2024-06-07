bcd0499d9d53fb9ffc2f0ac2840b732f11bdc44a
s1-haya <sunzecun@gmail.com>
2024-06-07 09:17:03 +0900

    Explain: run_test.py
    subprocessとは？？
    新しいプロセスを生成するモジュール。入力、出力、エラーパイプ接続し、終了コードを取得する。
    > The subprocess module allows you to spawn new processes,
    connect to their input/output/error pipes, and obtain their return codes.
    
    subprocessを使う理由
    Makefileを実行するため
    
    より応用してプロセスを生成したい場合はPopenってのもあるみたい。
    > The recommended approach to invoking subprocesses is to use the run() function for all use cases it can handle.
    For more advanced use cases, the underlying Popen interface can be used directly.
    
    subprocess.run(args, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None, **other_popen_kwargs)
    
    checkをtrueする理由
    ->　プロセスが0以外の終了コードで終了した場合、CalledProcessErrorという例外処理が投げられる。
    例外値は引数、終了ステータス、標準出力、標準エラーを保持する。
    > If check is true, and the process exits with a non-zero exit code, a CalledProcessError exception will be raised.
    Attributes of that exception hold the arguments, the exit code, and stdout and stderr if they were captured.

--- test/run_test.py
+++ test/run_test.py
@@ -2,14 +2,11 @@
 
 import subprocess
 
-def run_pytest():
-    try:
-      result = subprocess.run(['pytest'], check=True, capture_output=True, stderr=subprocess.PIPE)
-      stderr_output = result.stderr.decode()
-      print(stderr_output)
-    except subprocess.CalledProcessError as e:
-      print(f"pytest failed: {e}")
-      print(e.stderr.decode())
+def run_test():
+	try:
+		subprocess.run(['pytest'], check=True)
+	except subprocess.CalledProcessError as e:
+		print(f"pytest failed with exit code {e.returncode}")
 
 if __name__ == "__main__":
     run_pytest()
