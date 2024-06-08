bf19599d5765ed4fc5b7ae3efb2de971924fa543
habvi <93956895+habvi@users.noreply.github.com>
2024-06-09 02:55:59 +0900

    [fix] split / delimiterが空文字列の場合にearly returnを追加

--- srcs/utils/split.cpp
+++ srcs/utils/split.cpp
@@ -5,6 +5,10 @@ namespace utils {
 	SplitStr(const std::string &str, const std::string &delim) {
 		std::vector<std::string> split_str;
 
+		if (delim.size() == 0) {
+			split_str.push_back(str);
+			return split_str;
+		}
 		std::size_t start = 0;
 		while (start < str.size()) {
 			const std::string::size_type pos = str.find(delim, start);
