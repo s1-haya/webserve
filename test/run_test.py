4d6e6902b768e42b2b9edf31e60021a2998374ee
s1-haya <sunzecun@gmail.com>
2024-06-08 09:30:57 +0900

    Test: add exit(1) in run_test.py
    Whether the actions confirm that the test passed with an exit status code

--- test/run_test.py
+++ test/run_test.py
@@ -5,6 +5,7 @@ import subprocess
 def run_test():
 	try:
 		subprocess.run(['pytest'], check=True)
+		exit(1)
 	except subprocess.CalledProcessError as e:
 		print(f"pytest failed with exit code {e.returncode}")
 
