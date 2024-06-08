d314d558ce3997cd16a07dd770f81ed0c9e0cb70
habvi <93956895+habvi@users.noreply.github.com>
2024-06-08 20:17:21 +0900

    [add] client / test用clientを実行するためのmain,Makefile追加

---
+++ test/client/main.cpp
@@ -0,0 +1,30 @@
+#include "client.hpp"
+#include <cstdlib>
+#include <cstring> // strlen
+#include <iostream>
+
+namespace {
+	static const std::string COLOR_RED   = "\033[31m";
+	static const std::string COLOR_RESET = "\033[0m";
+
+	void PrintError(const std::string &s) {
+		std::cerr << COLOR_RED << "Error: " << s << COLOR_RESET << std::endl;
+	}
+} // namespace
+
+// default message  : ./client
+// original message : ./client "request message"
+int main(int argc, char **argv) {
+	try {
+		Client client;
+		if (argc > 1) {
+			const std::string message = std::string(argv[1], std::strlen(argv[1]));
+			client.SetMessage(message);
+		}
+		client.SendRequestAndReceiveResponse();
+	} catch (const std::exception &e) {
+		PrintError(e.what());
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
