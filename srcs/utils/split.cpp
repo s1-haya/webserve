25e121adbd3788a731e6d35bde9fa1b4a6878ea8
habvi <93956895+habvi@users.noreply.github.com>
2024-06-09 03:16:30 +0900

    [clean] split / 何度も呼ぶ必要のないsize()を変数に入れた

--- srcs/utils/split.cpp
+++ srcs/utils/split.cpp
@@ -4,20 +4,22 @@ namespace utils {
 	std::vector<std::string>
 	SplitStr(const std::string &src, const std::string &substring) {
 		std::vector<std::string> split_str;
+		const std::size_t        src_size    = src.size();
+		const std::size_t        substr_size = substring.size();
 
-		if (substring.size() == 0) {
+		if (substr_size == 0) {
 			split_str.push_back(src);
 			return split_str;
 		}
 		std::size_t start = 0;
-		while (start <= src.size()) {
+		while (start <= src_size) {
 			const std::string::size_type pos = src.find(substring, start);
 			if (pos == std::string::npos) {
 				split_str.push_back(src.substr(start));
 				break;
 			}
 			split_str.push_back(src.substr(start, pos - start));
-			start = pos + substring.size();
+			start = pos + substr_size;
 		}
 		return split_str;
 	}
