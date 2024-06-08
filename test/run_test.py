7800bcb8df6180515d3a9b3f3c451d58446691bb
s1-haya <sunzecun@gmail.com>
2024-06-08 09:32:48 +0900

    Update: add exit(1) to except in runt_test.py
    前回のコミットメッセージでactionsが終了ステータスを見てテストが通ったかどうか確認してるため
    ============================== 8 passed in 0.03s ===============================
    Error: Process completed with exit code 1.

--- test/run_test.py
+++ test/run_test.py
@@ -5,9 +5,9 @@ import subprocess
 def run_test():
 	try:
 		subprocess.run(['pytest'], check=True)
-		exit(1)
 	except subprocess.CalledProcessError as e:
 		print(f"pytest failed with exit code {e.returncode}")
+		exit(1)
 
 if __name__ == "__main__":
     run_test()
