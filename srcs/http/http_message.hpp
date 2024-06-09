df3e136a2456a768c1a9aa86e31488e29a4c2682
habvi <93956895+habvi@users.noreply.github.com>
2024-06-10 02:10:41 +0900

    [update] http-message / http-messageとして使う文字列を追加で定義

--- srcs/http/http_message.hpp
+++ srcs/http/http_message.hpp
@@ -5,5 +5,8 @@
 
 extern const std::string SP;
 extern const std::string CRLF;
+extern const std::string HTTP_VERSION;
+extern const std::string CONNECTION;
+extern const std::string CONTENT_LENGTH;
 
 #endif /* HTTP_MESSAGE_HPP_ */
