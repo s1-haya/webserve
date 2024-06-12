69fafd2036dab3f033cfe137c24cd3a1dc236f34
s1-haya <sunzecun@gmail.com>
2024-06-12 16:25:42 +0900

    Test: whether test_subprocess run success

--- test/integration/http/test_subprocess.py
+++ test/integration/http/test_subprocess.py
@@ -15,32 +15,32 @@ def run_command(command, working_dir):
         return None
 
 #コンテナの起動を待つ関数
-def wait_for_container(container_name, timeout=60, interval=10):
-    start_time = time.time()
-    while time.time() - start_time < timeout:
-        result = run_command(['make', 'ps'], webserv_dir)
-        if container_name in result.stdout:
-            print(f"Container {container_name} is up and running!")
-            return True
-        else:
-            print(f"Container {container_name} not ready, waiting...")
-        time.sleep(interval)
-    print(f"Container {container_name} did not start within the timeout period.")
-    return False
+# def wait_for_container(container_name, timeout=60, interval=10):
+#     start_time = time.time()
+#     while time.time() - start_time < timeout:
+#         result = run_command(['make', 'ps'], webserv_dir)
+#         if container_name in result.stdout:
+#             print(f"Container {container_name} is up and running!")
+#             return True
+#         else:
+#             print(f"Container {container_name} not ready, waiting...")
+#         time.sleep(interval)
+#     print(f"Container {container_name} did not start within the timeout period.")
+#     return False
 
 # イメージをビルドし、コンテナを起動
 run_command(['make', 'build'], webserv_dir)
 run_command(['make', 'run'], webserv_dir)
 
-if wait_for_container(container_name):
-    try:
-        # 起動後sleepしないとcurl: (56) Recv failure: Connection reset by peerになるため
-        time.sleep(5)
-        server_url = "http://localhost:8080"
-        cp = subprocess.run(['curl', server_url], capture_output=True, text=True)
-        print(cp.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Request failed: {e}")
+# if wait_for_container(container_name):
+try:
+    # 起動後sleepしないとcurl: (56) Recv failure: Connection reset by peerになるため
+    time.sleep(15)
+    server_url = "http://localhost:8080"
+    cp = subprocess.run(['curl', server_url], capture_output=True, text=True)
+    print(cp.stdout)
+except subprocess.CalledProcessError as e:
+    print(f"Request failed: {e}")
 
 # コンテナを停止し削除
 result = run_command(['make', 'fclean'], webserv_dir)
