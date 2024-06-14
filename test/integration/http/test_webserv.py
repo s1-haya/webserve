fac7bf7c5fcaa6327f3df01371bc030a184da471
s1-haya <sunzecun@gmail.com>
2024-06-14 12:29:49 +0900

    Fix: coderabbitで指摘された部分を修正

--- test/integration/http/test_webserv.py
+++ test/integration/http/test_webserv.py
@@ -1,13 +1,12 @@
 import subprocess
 import time
-import requests
 
-def test_wevserv():
+def test_webserv() -> None:
     try:
         # 起動後sleepしないとcurl: (56) Recv failure: Connection reset by peerになるため
         time.sleep(5)
         server_url = "http://localhost:8080"
-        cp = subprocess.run(['curl', server_url], capture_output=True, text=True)
+        cp = subprocess.run(['/usr/bin/curl', server_url], capture_output=True, text=True)
         # レスポンスのコンテントが出力されたらOK
         print(cp.stdout)
     except subprocess.CalledProcessError as e:
