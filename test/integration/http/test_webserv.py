62ffa822b3d4f53e3e37b49ab219c00347d0e38b
s1-haya <sunzecun@gmail.com>
2024-06-14 19:51:46 +0900

    Fix: Makefileでsleep5追加
    テストがコンテナの起動に依存してるコードだったためtest_webervでsleep5していたのをMakefilenに変更した

--- test/integration/http/test_webserv.py
+++ test/integration/http/test_webserv.py
@@ -1,10 +1,7 @@
 import subprocess
-import time
 
 def test_webserv() -> None:
     try:
-        # 起動後sleepしないとcurl: (56) Recv failure: Connection reset by peerになるため
-        time.sleep(5)
         server_url = "http://localhost:8080"
         cp = subprocess.run(['/usr/bin/curl', server_url], capture_output=True, text=True)
         # レスポンスのコンテントが出力されたらOK
