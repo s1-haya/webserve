319c0d4dc4f799d251987a01aced32a95105e4dd
林穎涛 <eitou.l@RinEitoMacBook-Air.local>
2024-06-16 16:20:40 +0900

    [update] 区切り文字とそれ以外に分けるようにLexerをアップデート

--- srcs/config_parse/lexer.hpp
+++ srcs/config_parse/lexer.hpp
@@ -8,45 +8,30 @@ class Lexer {
   private:
 	std::list<Node> &tokens_;
 	Lexer();
-	void               AddToken(const std::string& symbol, int token_type);
-	void               AddTokenIncrement(
-					  const std::string& token, int token_type, std::string::const_iterator &it
-				  );
-	void
-	AddTokenElem(const std::string& token, int token_type, std::string::const_iterator &it);
+
+	void AddToken(const char symbol, int token_type);
+	void AddToken(const std::string &symbol, int token_type);
+	void AddWordToken(std::string::const_iterator &it);
+	void SkipComment(std::string::const_iterator &it);
 	enum TokenType {
-		SERVER,
-		SERVER_NAME,
-		LOCATION,
 		DELIM,
-		SHARP,
 		L_BRACKET,
 		R_BRACKET,
-		LISTEN,
-		ROOT,
-		INDEX,
-		SLASH,
-		STRING,
+		WORD,
+		SHARP,
 	};
 	const std::string buffer_;
 
   public:
-	Lexer(const std::string &, std::list<Node>&);
+	Lexer(const std::string &, std::list<Node> &);
 	~Lexer();
-	void PrintTokens();
-	static const char CR = '\r';
-	static const char LF = '\n';
-	static const char DELIM_CHR = ';';
-	static const std::string SERVER_STR;
-	static const std::string SERVER_NAME_STR;
-	static const std::string LOCATION_STR;
-	static const std::string L_BRACKET_STR;
-	static const std::string R_BRACKET_STR;
-	static const std::string LISTEN_STR;
-	static const std::string ROOT_STR;
-	static const std::string INDEX_STR;
-	static const std::string SLASH_STR;
-	static const std::string SHARP_STR;
+	void              PrintTokens();
+	static const char CR            = '\r';
+	static const char LF            = '\n';
+	static const char DELIM_CHR     = ';';
+	static const char L_BRACKET_CHR = '{';
+	static const char R_BRACKET_CHR = '}';
+	static const char SHARP_CHR     = '#';
 };
 
 #endif
