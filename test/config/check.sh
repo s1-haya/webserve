10c5ad5fc6304a7d309418d744334067175b324a
林穎涛 <eitou.l@RinEitoMacBook-Air.local>
2024-06-19 16:51:25 +0900

    [update] ファイル名の変更、conf_fileをマウントしリアルタイムで書き換え可能に

--- test/config/check.sh
+++ test/config/check.sh
@@ -2,13 +2,13 @@
 
 if [ "$1" = "success" ]
 then
-	for file in /etc/nginx/success/*
+	for file in /etc/nginx/conf_file/success/*
 		do
 			nginx -t -c $file
 		done
 elif [ "$1" = "fail" ]
 then
-	for file in /etc/conf_file/fail/*
+	for file in /etc/nginx/conf_file/fail/*
 		do
 			nginx -t -c $file
 		done
