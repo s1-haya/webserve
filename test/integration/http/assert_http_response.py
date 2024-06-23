63aa1c803b5f5c4238d1acc772da1632d45c5082
s1-haya <sunzecun@gmail.com>
2024-06-23 11:50:09 +0000

    Update: add assert A == B, f("Expected A {A}, but got {B}")
    例外処理が投げられたときにどんな値がエラーしたかどうかを表示できるようにした
    E    AssertionError: Expected status code 404, but got 200

--- test/integration/http/assert_http_response.py
+++ test/integration/http/assert_http_response.py
@@ -2,17 +2,24 @@ from http import HTTPStatus
 from http.client import HTTPConnection
 from typing import Optional
 
-def assert_status_line(code: HTTPStatus, output: HTTPConnection) -> None:
-	assert code == output.status
-	assert code.phrase == output.reason
-	assert 11 == output.version
+def assert_status_line(response: HTTPConnection, expected_code: HTTPStatus) -> None:
+	assert response.status == expected_code, f"Expected status code {expected_code}, but got {response.status}"
+	assert response.reason == expected_code.phrase, f"Expected status code reason {expected_code.phrase}, but got {response.reason}"
+	assert response.version == 11, f"Expected version 1.1, but got {response.version}"
 
-def assert_header(name: Optional[str], value: Optional[str], output: HTTPConnection) -> None:
-	assert value == output.getheader(name).strip()
+def assert_header(response: HTTPConnection, expected_name: Optional[str], expected_header_value: Optional[str]) -> None:
+	response_header_value = response.getheader(expected_name)
+	# Header nameが存在するかどうか
+	if response_header_value is None:
+		raise ValueError(f"Header {expected_name} not found in the response")
+	# Header valueは末尾に空白があるためstrip()使用
+	response_header_value = response_header_value.strip()
+	assert response_header_value == expected_header_value , f"Expected header value {expected_header_value}, but got {response_header_value}"
 
-def assert_body(path: Optional[str], output: HTTPConnection) -> None:
+def assert_body(response: HTTPConnection, path: Optional[str]) -> None:
 	expected_body = b""
 	with open(path, "r") as f:
 		expected_body = f.read().encode("utf-8")
-	assert expected_body == output.read()
-	assert_header("Content-Length", str(len(expected_body)), output)
+	response_body = response.read()
+	assert response_body == expected_body, f"Expected response body {expected_body}, but got {response_body}"
+	assert_header(response, "Content-Length", str(len(expected_body)))
