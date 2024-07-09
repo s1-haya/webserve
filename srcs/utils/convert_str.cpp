d1745895ac2f8c386459529c95318a37da13bfc5
habvi <93956895+habvi@users.noreply.github.com>
2024-07-09 20:47:20 +0900

    [add] utils / port用にunsigned intとstrignの相互変換関数追加
    - utils追加ついでにisspaceファイル末の改行も追加

---
+++ srcs/utils/convert_str.cpp
@@ -0,0 +1,31 @@
+#include "utils.hpp"
+#include <cerrno>
+#include <cstdlib> // strtoul
+#include <limits>  // numeric_limits
+#include <sstream>
+
+namespace utils {
+
+bool ConvertStrToUint(const std::string &str, unsigned int &num) {
+	if (str.empty()) {
+		return false;
+	}
+
+	char            *end;
+	static const int BASE = 10;
+	errno                 = 0;
+	unsigned long tmp_num = std::strtoul(str.c_str(), &end, BASE);
+	if (errno == ERANGE || *end != '\0' || tmp_num > std::numeric_limits<unsigned int>::max()) {
+		return false;
+	}
+	num = static_cast<unsigned int>(tmp_num);
+	return true;
+}
+
+std::string ConvertUintToStr(unsigned int num) {
+	std::ostringstream oss;
+	oss << num;
+	return oss.str();
+}
+
+} // namespace utils
