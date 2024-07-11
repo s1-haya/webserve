6c05b0564b5fa70f9ccce34bf8aa8f387ec733f7
s1-haya <sunzecun@gmail.com>
2024-07-11 16:01:08 +0900

    "Update: リクエストライン、ヘッダフィールドとボディメッセージをsplitで分けた"

--- sandbox/sawa/http_parse/http_parse.cpp
+++ sandbox/sawa/http_parse/http_parse.cpp
@@ -49,8 +49,6 @@ HeaderFields HTTPParse::SetHeaderFields(const std::vector<std::string> &header_f
 	HeaderFields                                     header_fields;
 	typedef std::vector<std::string>::const_iterator It;
 	for (It it = header_fields_info.begin() + 1; it != header_fields_info.end(); ++it) {
-		if (*it == "")
-			break;
 		std::vector<std::string> header_key_value = utils::SplitStr(*it, ": ");
 		header_fields[header_key_value[0]]        = header_key_value[1];
 	}
@@ -59,7 +57,7 @@ HeaderFields HTTPParse::SetHeaderFields(const std::vector<std::string> &header_f
 
 // 　懸念点：MessageBodyにCRLFが含まれていた場合 std::vectorはLineごとに持ってる
 std::string HTTPParse::SetMessageBody(const std::vector<std::string> &message_body_info) {
-	std::vector<std::string>::const_iterator it           = message_body_info.end() - 1;
+	std::vector<std::string>::const_iterator it           = message_body_info.begin();
 	std::string                              message_body = *it;
 	return message_body;
 }
@@ -67,13 +65,18 @@ std::string HTTPParse::SetMessageBody(const std::vector<std::string> &message_bo
 // todo: tmp request_
 HTTPRequest HTTPParse::Run(const std::string &read_buf) {
 	HTTPRequest              request;
-	std::vector<std::string> lines = utils::SplitStr(read_buf, CRLF);
-	request.status_line            = SetRequestLine(utils::SplitStr(lines[0], SP));
-	request.header_fields          = SetHeaderFields(lines);
+	std::vector<std::string> a = utils::SplitStr(read_buf, CRLF + CRLF);
+	std::vector<std::string> b = utils::SplitStr(a[0], CRLF);
+	request.status_line        = SetRequestLine(utils::SplitStr(b[0], SP));
+	request.header_fields      = SetHeaderFields(b);
+	// 調査: bodymessageの取得方法（ヘッダーによって仕様が変わる）
 	if ("POST" == request.status_line.method)
-		request.message_body = SetMessageBody(lines);
-	// PrintLines(lines);
+		request.message_body = SetMessageBody(utils::SplitStr(a[1], ""));
+	// PrintLines(b);
 	return request;
 }
+
+// status_line && header
+// messagebody
 
 } // namespace http
