2ed80e96124470721d8a24b68d53b221618f2a1a
habvi <93956895+habvi@users.noreply.github.com>
2024-07-17 06:06:20 +0900

    [add] ClientInfo / getaddrinfo()を使用してport番号も取得

--- srcs/client_info.cpp
+++ srcs/client_info.cpp
@@ -1,4 +1,5 @@
 #include "client_info.hpp"
+#include <arpa/inet.h>  // ntohs
 #include <netdb.h>      // getnameinfo
 #include <netinet/in.h> // struct sock_addr,INET6_ADDRSTRLEN,ntohs
 #include <stdexcept>
@@ -10,7 +11,7 @@ namespace server {
 
 ClientInfo::ClientInfo(int fd, const struct sockaddr_storage &sock_addr) : fd_(fd), port_(0) {
 	SetSockInfo(sock_addr);
-	std::cout << "client (" << fd << ") " << "ip: " << ip_str_ << std::endl;
+	std::cout << "client (" << fd << ") " << "ip: " << ip_str_ << ", port: " << port_ << std::endl;
 }
 
 ClientInfo::~ClientInfo() {
@@ -19,6 +20,23 @@ ClientInfo::~ClientInfo() {
 	// 	close(fd_);
 	// }
 }
+
+namespace {
+
+unsigned int ConvertPortToHostByteOrder(const struct sockaddr_storage &sock_addr) {
+	// for IPv4
+	if (sock_addr.ss_family == AF_INET) {
+		return ntohs(((struct sockaddr_in *)&sock_addr)->sin_port);
+	}
+	// for IPv6
+	if (sock_addr.ss_family == AF_INET6) {
+		return ntohs(((struct sockaddr_in6 *)&sock_addr)->sin6_port);
+	}
+	// todo: tmp(unreachable?)
+	return -1;
+}
+
+} // namespace
 
 void ClientInfo::SetSockInfo(const struct sockaddr_storage &sock_addr) {
 	// getnameinfo
@@ -37,8 +55,9 @@ void ClientInfo::SetSockInfo(const struct sockaddr_storage &sock_addr) {
 		throw std::runtime_error("getnameinfo failed: " + std::string(gai_strerror(status)));
 	}
 
-	// set IP
+	// set IP,port
 	ip_str_ = ip_str;
+	port_   = ConvertPortToHostByteOrder(sock_addr);
 }
 
 } // namespace server
