1007ece83225b0b88a57ca459802b5943d95d6e0
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-07-23 19:05:02 +0900

    [update] LexerでWORDが連続しているかで処理を分岐することで、server_name server_name等に対応

--- srcs/config_parse/lexer.cpp
+++ srcs/config_parse/lexer.cpp
@@ -25,6 +25,8 @@ void Lexer::InitDefinition() {
 }
 
 void Lexer::LexBuffer() {
+	bool is_next_to_word = false;
+
 	for (std::string::const_iterator it = buffer_.begin(); it != buffer_.end(); ++it) {
 		while (utils::IsSpace(*it)) {
 			++it;
@@ -32,18 +34,26 @@ void Lexer::LexBuffer() {
 		switch (*it) {
 		case SEMICOLON_CHR:
 			AddToken(SEMICOLON_CHR, node::DELIM);
+			is_next_to_word = false;
 			break;
 		case L_BRACKET_CHR:
 			AddToken(L_BRACKET_CHR, node::L_BRACKET);
+			is_next_to_word = false;
 			break;
 		case R_BRACKET_CHR:
 			AddToken(R_BRACKET_CHR, node::R_BRACKET);
+			is_next_to_word = false;
 			break;
 		case SHARP_CHR:
 			SkipComment(it);
+			is_next_to_word = false;
 			break;
 		default:
-			AddWordToken(it);
+			if (is_next_to_word)
+				AddWordToken(it);
+			else
+				AddContextDirectiveWordToken(it);
+			is_next_to_word = true;
 			break;
 		}
 	}
@@ -63,6 +73,16 @@ void Lexer::AddToken(const std::string &symbol, node::TokenType token_type) {
 }
 
 void Lexer::AddWordToken(std::string::const_iterator &it) {
+	std::string new_str = "";
+	while (!utils::IsSpace(*it) && *it != SEMICOLON_CHR) {
+		new_str += *it;
+		++it;
+	}
+	AddToken(new_str, node::WORD);
+	--it; // loopでWordの次の文字から処理
+}
+
+void Lexer::AddContextDirectiveWordToken(std::string::const_iterator &it) {
 	std::string new_str = "";
 	while (!utils::IsSpace(*it) && *it != SEMICOLON_CHR) {
 		new_str += *it;
