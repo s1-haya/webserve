6b624dd216ae7152264bc6ceb35760fb11262a85
habvi <93956895+habvi@users.noreply.github.com>
2024-07-26 21:09:35 +0900

    [add] unit / getterを使用して期待通りVirtualServerが保存されているか・fdと紐づけられているかテスト

--- test/unit/virtual_server_storage/test_virtual_server_storage.cpp
+++ test/unit/virtual_server_storage/test_virtual_server_storage.cpp
@@ -1,10 +1,111 @@
+#include "color.hpp"
 #include "virtual_server.hpp"
 #include "virtual_server_storage.hpp"
 #include <cstdlib>
+#include <iostream>
+#include <sstream> // ostringstream
 
 namespace {
 
 typedef server::VirtualServer::LocationList LocationList;
+
+struct Result {
+	bool        is_ok;
+	std::string error_log;
+};
+
+int GetTestCaseNum() {
+	static int test_case_num = 0;
+	++test_case_num;
+	return test_case_num;
+}
+
+void PrintOk() {
+	std::cout << utils::color::GREEN << GetTestCaseNum() << ".[OK]" << utils::color::RESET
+			  << std::endl;
+}
+
+void PrintNg() {
+	std::cerr << utils::color::RED << GetTestCaseNum() << ".[NG] " << utils::color::RESET
+			  << std::endl;
+}
+
+void PrintError(const std::string &message) {
+	std::cerr << utils::color::RED << message << utils::color::RESET << std::endl;
+}
+
+template <typename T>
+bool IsSame(const T &result, const T &expected) {
+	return result == expected;
+}
+
+int Test(Result result) {
+	if (result.is_ok) {
+		PrintOk();
+		return EXIT_SUCCESS;
+	}
+	PrintNg();
+	PrintError(result.error_log);
+	return EXIT_FAILURE;
+}
+
+// -----------------------------------------------------------------------------
+// テストfail時のlocationsのdebug出力
+// (todo: test_virtual_server.cppと全く同じ関数なのでどうにかしても良いかも)
+std::string PrintLocations(const LocationList &locations) {
+	std::ostringstream oss;
+
+	typedef LocationList::const_iterator It;
+	for (It it = locations.begin(); it != locations.end(); ++it) {
+		oss << *it;
+		if (++LocationList::const_iterator(it) != locations.end()) {
+			oss << std::endl;
+		}
+	}
+	return oss.str();
+}
+
+Result
+TestIsSameVirtualServer(const server::VirtualServer &vs, const server::VirtualServer &expected_vs) {
+	Result result;
+	result.is_ok = true;
+	std::ostringstream oss;
+
+	// server_name
+	const std::string &server_name          = vs.GetServerName();
+	const std::string &expected_server_name = expected_vs.GetServerName();
+	if (!IsSame(server_name, expected_server_name)) {
+		result.is_ok = false;
+		oss << "server_name" << std::endl;
+		oss << "- result   [" << server_name << "]" << std::endl;
+		oss << "- expected [" << expected_server_name << "]" << std::endl;
+	}
+
+	// locations
+	const LocationList &locations          = vs.GetLocations();
+	const LocationList &expected_locations = expected_vs.GetLocations();
+	if (!IsSame(locations, expected_locations)) {
+		result.is_ok = false;
+		oss << "locations" << std::endl;
+		oss << "- result  " << std::endl;
+		oss << "[" << PrintLocations(locations) << "]" << std::endl;
+		oss << "- expected" << std::endl;
+		oss << "[" << PrintLocations(expected_locations) << "]" << std::endl;
+	}
+
+	result.error_log = oss.str();
+	return result;
+}
+
+Result RunGetVirtualServer(
+	const server::VirtualServerStorage &vs_storage,
+	int                                 server_fd,
+	const server::VirtualServer        &expected_vs
+) {
+	const server::VirtualServer &vs = vs_storage.GetVirtualServer(server_fd);
+
+	return TestIsSameVirtualServer(vs, expected_vs);
+}
 
 // -----------------------------------------------------------------------------
 // - virtual_serverは以下の想定
@@ -45,6 +146,11 @@ int RunTestVirtualServerStorage() {
 	vs_storage.AddMapping(4, &vs1);
 	vs_storage.AddMapping(5, &vs1);
 	vs_storage.AddMapping(6, &vs2);
+
+	// getterを使用して期待通りvirtual_serverが追加されてるか・紐づけられているかテスト
+	ret_code |= Test(RunGetVirtualServer(vs_storage, 4, vs1));
+	ret_code |= Test(RunGetVirtualServer(vs_storage, 5, vs1));
+	ret_code |= Test(RunGetVirtualServer(vs_storage, 6, vs2));
 
 	return ret_code;
 }
