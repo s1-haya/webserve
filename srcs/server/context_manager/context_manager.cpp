4fc18dea70a22eca037d743eb26f927fff27b756
habvi <93956895+habvi@users.noreply.github.com>
2024-08-07 04:27:27 +0900

    [update] context / context_managerがsock_contextクラスの関数をwrapする

--- srcs/server/context_manager/context_manager.cpp
+++ srcs/server/context_manager/context_manager.cpp
@@ -12,8 +12,35 @@ ContextManager::ContextManager(const ContextManager &other) {
 
 ContextManager &ContextManager::operator=(const ContextManager &other) {
 	if (this != &other) {
+		sock_context_ = other.sock_context_;
 	}
 	return *this;
+}
+
+void ContextManager::AddServerInfo(
+	int server_fd, const ServerInfo &server_info, const VirtualServer *virtual_server
+) {
+	(void)virtual_server;
+	sock_context_.AddServerInfo(server_fd, server_info);
+}
+
+void ContextManager::AddClientInfo(int client_fd, const ClientInfo &client_info, int server_fd) {
+	sock_context_.AddClientInfo(client_fd, client_info, server_fd);
+}
+
+void ContextManager::DeleteClientInfo(int client_fd) {
+	sock_context_.DeleteClientInfo(client_fd);
+}
+
+const ServerInfo &ContextManager::GetServerInfo(int client_fd) const {
+	const ServerInfo &server_info = sock_context_.GetConnectedServerInfo(client_fd);
+	return server_info;
+}
+
+// todo: IP以外も必要ならDTO作って詰めて返す
+const std::string &ContextManager::GetClientInfo(int client_fd) const {
+	const ClientInfo &client_info = sock_context_.GetClientInfo(client_fd);
+	return client_info.GetIp();
 }
 
 } // namespace server
--- srcs/server/context_manager/context_manager.hpp
+++ srcs/server/context_manager/context_manager.hpp
@@ -1,9 +1,12 @@
 #ifndef CONTEXT_MANAGER_HPP_
 #define CONTEXT_MANAGER_HPP_
 
+#include "sock_context.hpp"
 #include <string>
 
 namespace server {
+
+class VirtualServer;
 
 // holds and manages virtual server info and socket context(server socket info, client socket info).
 class ContextManager {
@@ -12,6 +15,18 @@ class ContextManager {
 	~ContextManager();
 	ContextManager(const ContextManager &other);
 	ContextManager &operator=(const ContextManager &other);
+	// functions
+	void AddServerInfo(
+		int server_fd, const ServerInfo &server_info, const VirtualServer *virtual_server
+	);
+	void AddClientInfo(int client_fd, const ClientInfo &client_info, int server_fd);
+	void DeleteClientInfo(int client_fd);
+	// getter
+	const ServerInfo  &GetServerInfo(int client_fd) const;
+	const std::string &GetClientInfo(int client_fd) const;
+
+  private:
+	SockContext sock_context_;
 };
 
 } // namespace server
