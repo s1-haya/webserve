2ff8cb4d09d16fb9ca5f2d9a5bfd748d1b54e73a
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-08-13 10:05:16 +0900

    [add] ディレクティブ名を定義するファイル追加

---
+++ srcs/config_parse/directive_names.cpp
@@ -0,0 +1,18 @@
+#include "directive_names.hpp"
+
+namespace config {
+
+const std::string SERVER   = "server";
+const std::string LOCATION = "location";
+
+const std::string SERVER_NAME          = "server_name";
+const std::string LISTEN               = "listen";
+const std::string ALIAS                = "alias";
+const std::string INDEX                = "index";
+const std::string AUTO_INDEX           = "autoindex";
+const std::string ERROR_PAGE           = "error_page";
+const std::string CLIENT_MAX_BODY_SIZE = "client_max_body_size";
+const std::string ALLOWED_METHODS      = "allowed_methods";
+const std::string RETURN               = "return";
+
+} // namespace config
---
+++ srcs/config_parse/directive_names.hpp
@@ -0,0 +1,23 @@
+#ifndef DIRECTIVE_NAMES_HPP_
+#define DIRECTIVE_NAMES_HPP_
+
+#include <string>
+
+namespace config {
+
+extern const std::string SERVER;
+extern const std::string LOCATION;
+
+extern const std::string SERVER_NAME;
+extern const std::string LISTEN;
+extern const std::string ALIAS;
+extern const std::string INDEX;
+extern const std::string AUTO_INDEX;
+extern const std::string ERROR_PAGE;
+extern const std::string CLIENT_MAX_BODY_SIZE;
+extern const std::string ALLOWED_METHODS;
+extern const std::string RETURN;
+
+} // namespace config
+
+#endif
