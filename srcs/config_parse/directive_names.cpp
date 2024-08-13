48cf2fe948c70e3003c2822b030b936375c83a10
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-08-13 10:35:16 +0900

    [add] 未実装のディレクティブ名も列挙

--- srcs/config_parse/directive_names.cpp
+++ srcs/config_parse/directive_names.cpp
@@ -5,14 +5,19 @@ namespace config {
 const std::string SERVER   = "server";
 const std::string LOCATION = "location";
 
-const std::string SERVER_NAME          = "server_name";
+const std::string HOST                 = "host";
 const std::string LISTEN               = "listen";
-const std::string ALIAS                = "alias";
-const std::string INDEX                = "index";
-const std::string AUTO_INDEX           = "autoindex";
+const std::string SERVER_NAME          = "server_name";
 const std::string ERROR_PAGE           = "error_page";
 const std::string CLIENT_MAX_BODY_SIZE = "client_max_body_size";
-const std::string ALLOWED_METHODS      = "allowed_methods";
-const std::string RETURN               = "return";
+
+const std::string ALLOWED_METHODS = "allowed_methods";
+const std::string RETURN          = "return";
+const std::string ALIAS           = "alias";
+const std::string AUTO_INDEX      = "autoindex";
+const std::string INDEX           = "index";
+
+const std::string CGI_EXTENSION = "cgi_extension";
+const std::string UPLOAD_DIR    = "upload_dir";
 
 } // namespace config
--- srcs/config_parse/directive_names.hpp
+++ srcs/config_parse/directive_names.hpp
@@ -14,19 +14,38 @@ extern const std::string SERVER;
 extern const std::string LOCATION;
 
 /**
- * @brief Directive in Context
+ * @brief Directive in Server Context
+ * @details Dir_name args;
+ *
+ * @note Host dir Not implemented yet
+ */
+
+extern const std::string HOST;
+extern const std::string LISTEN;
+extern const std::string SERVER_NAME;
+extern const std::string ERROR_PAGE;
+extern const std::string CLIENT_MAX_BODY_SIZE;
+
+/**
+ * @brief Directive in Location Context
  * @details Dir_name args;
  */
 
-extern const std::string SERVER_NAME;
-extern const std::string LISTEN;
-extern const std::string ALIAS;
-extern const std::string INDEX;
-extern const std::string AUTO_INDEX;
-extern const std::string ERROR_PAGE;
-extern const std::string CLIENT_MAX_BODY_SIZE;
 extern const std::string ALLOWED_METHODS;
 extern const std::string RETURN;
+extern const std::string ALIAS;
+extern const std::string AUTO_INDEX;
+extern const std::string INDEX;
+
+/**
+ * @brief Directive in Location Context for CGI
+ * @details Dir_name args;
+ *
+ * @note Not Implemented yet
+ */
+
+extern const std::string CGI_EXTENSION;
+extern const std::string UPLOAD_DIR;
 
 } // namespace config
 
