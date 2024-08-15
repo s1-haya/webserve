46c16ddf99b39f9edbc4a69fe82ed3b2fe54ebee
s1-haya <sunzecun@gmail.com>
2024-08-15 14:40:22 +0900

    Add: test case for get method

--- test/unit/method/test_method.cpp
+++ test/unit/method/test_method.cpp
@@ -1,20 +1,70 @@
 #include "http_response.hpp"
+#include "utils.hpp"
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 
+namespace {
+
+std::string LoadFileContent(const std::string &file_path) {
+	std::ifstream file(file_path.c_str());
+	if (!file) {
+		std::cerr << "Error opening file: " << file_path << std::endl;
+		return "";
+	}
+	std::ostringstream file_content;
+	file_content << file.rdbuf();
+	return file_content.str();
+}
+
+int GetTestCaseNum() {
+	static unsigned int test_case_num = 0;
+	++test_case_num;
+	return test_case_num;
+}
+
+template <typename T>
+int HandleResult(const T &result, const T &expected) {
+	if (result == expected) {
+		std::cout << utils::color::GREEN << GetTestCaseNum() << ".[OK]" << utils::color::RESET
+				  << std::endl;
+		return EXIT_SUCCESS;
+	} else {
+		std::cerr << utils::color::RED << GetTestCaseNum() << ".[NG] " << utils::color::RESET
+				  << std::endl;
+		return EXIT_FAILURE;
+	}
+}
+} // namespace
+
 int main(void) {
+	int ret_code = EXIT_SUCCESS;
+
+	// CF -> success or error_pages directive
+	// CRLF -> default error file
+
+	// CF
 	std::string makefile;
+	std::string expected_makefile = LoadFileContent("expected/makefile.txt");
 	http::HttpResponse::GetHandler("Makefile", makefile);
-	std::cout << makefile << std::endl;
+	ret_code = HandleResult(makefile, expected_makefile);
 
+	// CRLF
 	std::string not_found;
-	http::HttpResponse::GetHandler("a", not_found);
-	std::cout << not_found << std::endl;
+	std::string expected_not_found = LoadFileContent("expected/not_found.txt");
+	http::HttpResponse::GetHandler("test/a", not_found);
+	ret_code = HandleResult(not_found, expected_not_found);
 
+	// CRLF
 	std::string redirect;
-	http::HttpResponse::GetHandler("../../../test/unit", redirect);
-	std::cout << redirect << std::endl;
+	std::string expected_redirect = LoadFileContent("expected/redirect.txt");
+	http::HttpResponse::GetHandler("test/directory", redirect);
+	ret_code = HandleResult(redirect, expected_redirect);
 
-	std::string forbidden;
-	http::HttpResponse::GetHandler("forbidden_file", forbidden);
-	std::cout << forbidden << std::endl;
+	// CRLF 権限のないファイルをaddすることができなかったためローカルで各自テストしてください
+	// std::string forbidden;
+	// std::string expected_forbidden = LoadFileContent("expected/forbidden.txt");
+	// http::HttpResponse::GetHandler("test/forbidden_file", forbidden);
+	// ret_code = HandleResult(forbidden, expected_forbidden);
+	return ret_code;
 }
