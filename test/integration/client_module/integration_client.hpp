d0781940b327759ca2a693046853e061edb3f7bc
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-08-24 20:04:29 +0900

    [add] pybind11を用いてpythonにClient.cppを取り入れたtest_integration.pyを作成

---
+++ test/integration/client_module/integration_client.hpp
@@ -0,0 +1,29 @@
+#ifndef TEST_CLIENT_HPP_
+#define TEST_CLIENT_HPP_
+
+#include <netinet/in.h> // struct sockaddr_in
+#include <string>
+
+class Client {
+  public:
+	explicit Client(unsigned int port);
+	~Client();
+	std::string SendRequestAndReceiveResponse(const std::string &message);
+	int         test();
+
+  private:
+	Client();
+	// prohibit copy
+	Client(const Client &other);
+	Client &operator=(const Client &other);
+	void    Init();
+	// const
+	static const int          SYSTEM_ERROR = -1;
+	static const unsigned int BUFFER_SIZE  = 1024;
+	// socket
+	unsigned int       port_;
+	int                sock_fd_;
+	struct sockaddr_in sock_addr_;
+};
+
+#endif /* TEST_CLIENT_HPP_ */
---
+++ test/integration/test_integration.py
@@ -0,0 +1,16 @@
+import client_module.client as client
+
+def test_webserv() -> None:
+    try:
+        client_instance = client.Client(8080)
+        print(dir(client_instance))
+        response = client_instance.SendRequestAndReceiveResponse("GET / HTTP/1.1\nHost: localhost\nConnection: close\r\n\r\n")
+        print(response)
+        
+    except Exception as e:
+        print(e)
+        assert False
+
+
+if __name__ == "__main__":
+    test_webserv()
