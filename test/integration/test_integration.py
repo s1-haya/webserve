016f0ccd06b70d4dfb8b05a5c7d1a3c75c0155a8
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-08-28 13:39:40 +0900

    [add] エラーケース追加

--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -14,26 +14,68 @@ def build_client_module():
     )
 
 
+try:
+    # client_module をビルド
+    build_client_module()
+
+    # client_module.client をインポート
+    import client_module.client as client
+except subprocess.CalledProcessError as e:
+    print(f"Build failed: {e}")
+except ImportError as e:
+    print(f"Import failed: {e}")
+
+
 def test_webserv():
     try:
-        # client_module をビルド
-        build_client_module()
+        # test1()
+        # test2()
+        # test3()
+        test4()
+        # test5()
+    except Exception as e:
+        print(f"Test failed: {e}")
 
-        # client_module.client をインポート
-        import client_module.client as client
 
-        # Client クラスの使用例
-        client_instance = client.Client(8080)
+def test1():
+    client_instance = client.Client(8080)
+    response = client_instance.SendRequestAndReceiveResponse("")
+    print(response)
+    # assert
+
+
+def test2():
+    client_instance = client.Client(8080)
+    response = client_instance.SendRequestAndReceiveResponse("GET / HTTP/1.1\n")
+    print(response)
+    # assert
+
+
+def test3():
+    client_instance = client.Client(8080)
+    response = client_instance.SendRequestAndReceiveResponse("aaa" * 10000)
+    print(response)
+    # assert
+
+
+def test4():
+    client_instance = client.Client(8080)
+    while True:
         response = client_instance.SendRequestAndReceiveResponse(
-            "GET / HTTP/1.1\nHost: localhost\nConnection: close\r\n\r\n"
+            "GET / HTTP/1.1\nHost: localhost\r\n\r\n"
         )
-        print(response)
-
-    except subprocess.CalledProcessError as e:
-        print(f"Build failed: {e}")
-    except ImportError as e:
-        print(f"Import failed: {e}")
+    print(response)
+    # assert
 
 
-if __name__ == "__main__":
-    test_webserv()
+def test5():
+    client_instance = client.Client(8080)
+    response = client_instance.SendRequestAndReceiveResponse(
+        "GET \n   Test: aaa  \r\n\r\n"
+    )
+    print(response)
+    # assert
+
+
+# if __name__ == "__main__":
+#     test_webserv()
