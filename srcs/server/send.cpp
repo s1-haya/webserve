dbcf108d14020c4be34088a55faad8335ce990f7
habvi <93956895+habvi@users.noreply.github.com>
2024-08-31 22:32:11 +0900

    [style] send / 不要なinclude削除,変数名変更,返り値の説明comment追加

--- srcs/server/send.cpp
+++ srcs/server/send.cpp
@@ -2,8 +2,6 @@
 #include <cstdio>       // perror
 #include <sys/socket.h> // send
 
-#include <iostream>
-
 namespace server {
 
 Send::Send() {}
@@ -13,13 +11,15 @@ Send::~Send() {}
 Send::SendResult Send::SendResponse(int client_fd, const Response &response) {
 	SendResult send_result;
 
-	ssize_t send_ret = send(client_fd, response.c_str(), response.size(), 0);
-	if (send_ret == SYSTEM_ERROR) {
+	ssize_t send_size = send(client_fd, response.c_str(), response.size(), 0);
+	if (send_size == SYSTEM_ERROR) {
 		perror("send failed");
 		send_result.Set(false);
 		return send_result;
 	}
-	const Response unsent_response = response.substr(send_ret);
+	// Returns the substring starting from the send_size-th character.
+	// If the entire response was sent, an empty string is returned.
+	const Response unsent_response = response.substr(send_size);
 	send_result.SetValue(unsent_response);
 	return send_result;
 }
