b7f76786a83eaa4d1db1dbe16a76ca3dad756554
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-09-04 07:57:26 +0900

    [update] host_portをpairに変更(テスト未変更)

--- srcs/config_parse/parser.hpp
+++ srcs/config_parse/parser.hpp
@@ -35,7 +35,7 @@ class Parser {
 	 */
 
 	void HandleServerName(std::list<std::string> &server_names, NodeItr &it);
-	void HandleListen(std::string &host, context::PortList &port, NodeItr &it);
+	void HandleListen(context::HostPortPair &host_port, NodeItr &it);
 	void HandleClientMaxBodySize(std::size_t &client_max_body_size, NodeItr &it);
 	void HandleErrorPage(std::pair<unsigned int, std::string> &error_page, NodeItr &it);
 
