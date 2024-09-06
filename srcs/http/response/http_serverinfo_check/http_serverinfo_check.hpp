d1bba96515fd414e68e7356a43446c7fa38f290c
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-09-06 09:27:05 +0900

    [add] VSListのチェックに対応した関数追加

--- srcs/http/response/http_serverinfo_check/http_serverinfo_check.hpp
+++ srcs/http/response/http_serverinfo_check/http_serverinfo_check.hpp
@@ -7,6 +7,8 @@
 #include <list>
 #include <string>
 
+#include "virtual_server.hpp"
+
 namespace http {
 
 struct CheckServerInfoResult {
@@ -21,13 +23,15 @@ struct CheckServerInfoResult {
 
 	utils::Result< std::pair<unsigned int, std::string> > redirect;
 	std::pair<unsigned int, std::string>                  error_page;
-	CheckServerInfoResult() : autoindex(false){};
+	CheckServerInfoResult() : autoindex(false) {};
 };
 
 class HttpServerInfoCheck {
   private:
 	HttpServerInfoCheck();
 	~HttpServerInfoCheck();
+
+	/*====================================================*/ // 消す
 
 	static void CheckDTOServerInfo(
 		CheckServerInfoResult    &result,
@@ -52,6 +56,40 @@ class HttpServerInfoCheck {
 	static void CheckCgiExtension(CheckServerInfoResult &result, const MockLocationCon &location);
 	static void
 	CheckUploadDirectory(CheckServerInfoResult &result, const MockLocationCon &location);
+
+	/*====================================================*/ // 消す
+
+	typedef std::list<const server::VirtualServer &> VirtualServerAddrList;
+	static const server::VirtualServer &
+	CheckVSList(const VirtualServerAddrList &server_infos, HttpRequestFormat &request);
+	static CheckServerInfoResult
+				Check(const VirtualServerAddrList &server_infos, HttpRequestFormat &request);
+	static void CheckDtoServerInfo(
+		CheckServerInfoResult       &result,
+		const server::VirtualServer &virtual_server,
+		HeaderFields                &header_fields
+	);
+
+	typedef std::list<server::Location> VSLocationList;
+	static void                         CheckLocationList(
+								CheckServerInfoResult &result,
+								const VSLocationList  &locations,
+								const std::string     &request_target
+							);
+	static const server::Location CheckLocation(
+		CheckServerInfoResult &result,
+		const VSLocationList  &locations,
+		const std::string     &request_target
+	);
+	static void CheckIndex(CheckServerInfoResult &result, const server::Location &location);
+	static void CheckAutoIndex(CheckServerInfoResult &result, const server::Location &location);
+	static void CheckAlias(CheckServerInfoResult &result, const server::Location &location);
+	static void CheckRedirect(CheckServerInfoResult &result, const server::Location &location);
+	static void
+	CheckAllowedMethods(CheckServerInfoResult &result, const server::Location &location);
+	static void CheckCgiExtension(CheckServerInfoResult &result, const server::Location &location);
+	static void
+	CheckUploadDirectory(CheckServerInfoResult &result, const server::Location &location);
 
   public:
 	static CheckServerInfoResult
