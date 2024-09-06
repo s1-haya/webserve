27899f18cc2cd455b98208a67e6cdbf74021cce6
sawamura-hayato <sunzecun@gmail.com>
2024-09-06 14:31:12 +0000

    Update: method test

--- test/unit/http_method/test_http_method.cpp
+++ test/unit/http_method/test_http_method.cpp
@@ -1,12 +1,30 @@
 #include "http_exception.hpp"
 #include "http_message.hpp"
 #include "http_method.hpp"
+#include "http_response.hpp"
 #include "utils.hpp"
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 
 namespace {
+
+typedef std::list<std::string> AllowMethods;
+
+struct MethodArgument {
+	MethodArgument(
+		const std::string            &path,
+		const std::string            &method,
+		const AllowMethods &allow_methods,
+		const std::string            &request_body_message,
+		std::string                  &response_body_message
+	) : path(path), method(method), allow_methods(allow_methods), request_body_message(request_body_message), response_body_message(response_body_message) {}
+	const std::string &path;
+	const std::string            &method;
+	const AllowMethods &allow_methods;
+	const std::string            &request_body_message;
+	std::string                  &response_body_message;
+};
 
 std::string LoadFileContent(const std::string &file_path) {
 	std::ifstream file(file_path.c_str());
@@ -38,65 +56,35 @@ int HandleResult(const T &result, const T &expected) {
 	}
 }
 
-void TryGetHandler(const std::string &path, std::string &response_body_message) {
+int MethodHandlerResult(const MethodArgument& srcs, const std::string &expected_body_message) {
+	int result = 0;
 	try {
-		http::Method::GetHandler(path, response_body_message);
-	} catch (const http::HttpException &e) {
-		// ステータスコードが300番台以上の場合
-		// std::cerr << e.what() << std::endl;
-	}
-}
+		http::Method::Handler(
+			srcs.path, srcs.method, srcs.allow_methods, srcs.request_body_message, srcs.response_body_message
+		);
+		result = HandleResult(srcs.response_body_message, expected_body_message);
 
-void TryPostHandler(
-	const std::string &path, std::string &request_body_message, std::string &response_body_message
-) {
-	try {
-		http::Method::PostHandler(path, request_body_message, response_body_message);
-	} catch (const http::HttpException &e) {
-		// ステータスコードが300番台以上の場合
-		// std::cerr << e.what() << std::endl;
+	} catch (const http::HttpException& e) {
+		srcs.response_body_message = http::HttpResponse::CreateDefaultBodyMessageFormat(e.GetStatusCode());
+		result = HandleResult(srcs.response_body_message, expected_body_message);
+		std::cerr << utils::color::GRAY << e.what() << utils::color::RESET
+									 << std::endl;
 	}
-}
-
-void TryDeleteHandler(const std::string &path, std::string &response_body_message) {
-	try {
-		http::Method::DeleteHandler(path, response_body_message);
-	} catch (const http::HttpException &e) {
-		// ステータスコードが300番台以上の場合
-		// std::cerr << e.what() << std::endl;
-	}
+	return result;
 }
 
 } // namespace
 
-// StatusCode TryMethodHandler(
-// 		const std::string &path,
-// 		const std::string &method,
-// 		std::string       &request_body_message,
-// 		std::string       &response_body_message
-// 	) {
-// 	try {
-// 		if (method == GET) {
-// 			return HttpResponse::GetHandler(path, response_body_message);
-// 		} else if (method == POST) {
-// 			return HttpResponse::PostHandler(path, request_body_message, response_body_message);
-// 		} else if (method == DELETE) {
-// 			rTryDeleteHandler(path, response_body_message);
-// 		} else {
-// 			StatusCode status_code      = StatusCode(NOT_IMPLEMENTED);
-// 			response_body_message            =
-// HttpResponse::CreateDefaultBodyMessageFormat(status_code); 			throw HttpException("Error:
-// Not Implemented", status_code);
-// 		}
-// 	}
-// 	catch (const HttpException &e) {
-// 		// ステータスコードが300番台以上の場合
-// 		return (e.GetStatusCode());
-// 	}
-// }
-
 int main(void) {
 	int ret_code = EXIT_SUCCESS;
+
+	// method test init
+	AllowMethods allow_methods;
+	allow_methods.push_back(http::GET);
+	allow_methods.push_back(http::POST);
+	allow_methods.push_back(http::DELETE);
+	std::string request;
+	std::string response;
 
 	// http_method/expected
 	// LF:   exist target resourse file
@@ -108,76 +96,94 @@ int main(void) {
 	std::string expected_forbidden  = LoadFileContent("expected/forbidden.txt");
 	std::string expected_not_found  = LoadFileContent("expected/not_found.txt");
 
+	// GET test
 	// ファイルが存在する場合
-	std::string get_test1_response_body_message;
-	TryGetHandler("test/file.txt", get_test1_response_body_message);
-	ret_code |= HandleResult(get_test1_response_body_message, expected_file);
+	ret_code |= MethodHandlerResult(MethodArgument("test/file.txt", http::GET, allow_methods, request, response), expected_file);
 
 	// ファイルが存在しない場合
-	std::string get_test2_response_body_message;
-	TryGetHandler("test/a", get_test2_response_body_message);
-	ret_code |= HandleResult(get_test2_response_body_message, expected_not_found);
+	ret_code |= MethodHandlerResult(
+		MethodArgument("test/a", http::GET, allow_methods, request, response), expected_not_found
+	);
 
 	// ディレクトリの場合かつ'/'がない場合
-	std::string get_test3_response_body_message;
-	TryGetHandler("test/directory", get_test3_response_body_message);
-	ret_code |= HandleResult(get_test3_response_body_message, expected_redirect);
+	ret_code |= MethodHandlerResult(
+		MethodArgument("test/directory", http::GET, allow_methods, request, response), expected_redirect
+	);
 
 	// ファイルが権限ない場合
-	std::string get_test4_response_body_message;
-	TryGetHandler("test/no_authority_file", get_test4_response_body_message);
-	ret_code |= HandleResult(get_test4_response_body_message, expected_forbidden);
+	ret_code |= MethodHandlerResult(
+		MethodArgument("test/no_authority_file", http::GET, allow_methods, request, response),
+		expected_forbidden
+	);
 
 	// POST test
 	// 新しいファイルをアップロードする場合
-	std::string post_test1_request_body_message = "OK";
-	std::string post_test1_response_body_message;
-	TryPostHandler("ok.txt", post_test1_request_body_message, post_test1_response_body_message);
-	ret_code |= HandleResult(post_test1_response_body_message, expected_created);
+	const std::string& post_test1_request_body_message = "OK";
+	ret_code |= MethodHandlerResult(
+		MethodArgument("ok.txt", http::POST, allow_methods, post_test1_request_body_message, response),
+		expected_created
+	);
 
 	// すでにアップロードされたファイルをアップロードする場合
-	std::string post_test2_request_body_message = "OK";
-	std::string post_test2_response_body_message;
-	TryPostHandler("ok.txt", post_test2_request_body_message, post_test2_response_body_message);
-	ret_code |= HandleResult(post_test2_response_body_message, expected_no_content);
+	const std::string& post_test2_request_body_message = "OK";
+	ret_code |= MethodHandlerResult(
+		MethodArgument("ok.txt", http::POST, allow_methods, post_test2_request_body_message, response),
+		expected_no_content
+	);
 
 	// ディレクトリの場合
-	std::string post_test3_request_body_message;
-	std::string post_test3_response_body_message;
-	TryPostHandler(
-		"test/directory", post_test3_request_body_message, post_test3_response_body_message
+	ret_code |= MethodHandlerResult(
+		MethodArgument(
+			"test/directory", http::POST, allow_methods, request, response
+		),
+		expected_forbidden
 	);
-	ret_code |= HandleResult(post_test3_response_body_message, expected_forbidden);
 
 	// DELETE test
 	// ファイルが存在するかつ親ディレクトリが書き込み権限あるとき
-	std::string delete_test1_response_body_message;
-	TryDeleteHandler("ok.txt", delete_test1_response_body_message);
-	ret_code |= HandleResult(delete_test1_response_body_message, expected_no_content);
+	ret_code |= MethodHandlerResult(
+		MethodArgument(
+			"ok.txt", http::DELETE, allow_methods, request, response
+		),
+		expected_no_content
+	);
 
 	// ファイルが存在しない場合
-	std::string delete_test2_response_body_message;
-	TryDeleteHandler("not_found.txt", delete_test2_response_body_message);
-	ret_code |= HandleResult(delete_test2_response_body_message, expected_not_found);
+	ret_code |= MethodHandlerResult(
+		MethodArgument(
+			"not_found.txt", http::DELETE, allow_methods, request, response
+		),
+		expected_not_found
+	);
 
 	// ディレクトリ内にファイルが存在してる場合
-	std::string delete_test3_response_body_message;
-	TryDeleteHandler("test", delete_test3_response_body_message);
-	ret_code |= HandleResult(delete_test3_response_body_message, expected_forbidden);
+	ret_code |= MethodHandlerResult(
+		MethodArgument(
+			"test", http::DELETE, allow_methods, request, response
+		),
+		expected_forbidden
+	);
 
-	// 　ディレクトリ内にファイルが存在してない場合
-	std::string delete_test4_response_body_message;
-	TryDeleteHandler("test/s", delete_test4_response_body_message);
-	ret_code |= HandleResult(delete_test4_response_body_message, expected_forbidden);
+	// ディレクトリ内にファイルが存在してない場合
+	ret_code |= MethodHandlerResult(
+		MethodArgument(
+			"test/s", http::DELETE, allow_methods, request, response
+		),
+		expected_forbidden
+	);
 
 	// 存在しないディレクトリの場合
-	std::string delete_test5_response_body_message;
-	TryDeleteHandler("not_found_directory", delete_test5_response_body_message);
-	ret_code |= HandleResult(delete_test5_response_body_message, expected_not_found);
+	ret_code |= MethodHandlerResult(
+		MethodArgument("not_found_directory", http::DELETE, allow_methods, request, response),
+		expected_not_found
+	);
 
 	// 書き込み権限がないディレクトリの中にあるファイル場合
-	std::string delete_test6_response_body_message;
-	TryDeleteHandler("test/no_authority_directory/test.txt", delete_test6_response_body_message);
-	ret_code |= HandleResult(delete_test6_response_body_message, expected_forbidden);
+	ret_code |= MethodHandlerResult(
+		MethodArgument(
+			"test/no_authority_directory/test.txt", http::DELETE, allow_methods, request, response
+		),
+		expected_forbidden
+	);
 	return ret_code;
 }
