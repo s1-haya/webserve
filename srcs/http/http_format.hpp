1506484e30fccbf86debc9ef35a21ca512f5528b
sawamura-hayato <sunzecun@gmail.com>
2024-09-07 20:05:57 +0900

    Style format

--- srcs/http/http_format.hpp
+++ srcs/http/http_format.hpp
@@ -13,8 +13,10 @@ struct RequestLine {
 };
 
 struct StatusLine {
-	StatusLine(const std::string& version, const std::string& status_code, const std::string& reason_phrase) :
-	 version(version), status_code(status_code), reason_phrase(reason_phrase) {};
+	StatusLine(
+		const std::string &version, const std::string &status_code, const std::string &reason_phrase
+	)
+		: version(version), status_code(status_code), reason_phrase(reason_phrase) {};
 	std::string version;
 	std::string status_code;
 	std::string reason_phrase;
@@ -29,7 +31,12 @@ struct HttpRequestFormat {
 };
 
 struct HttpResponseFormat {
-	HttpResponseFormat(const StatusLine& status_line, const HeaderFields& header_fields, const std::string& body_message) : status_line(status_line), header_fields(header_fields), body_message(body_message) {};
+	HttpResponseFormat(
+		const StatusLine   &status_line,
+		const HeaderFields &header_fields,
+		const std::string  &body_message
+	)
+		: status_line(status_line), header_fields(header_fields), body_message(body_message) {};
 	StatusLine   status_line;
 	HeaderFields header_fields;
 	std::string  body_message;
