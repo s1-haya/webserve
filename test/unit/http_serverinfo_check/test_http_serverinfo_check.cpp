dda3410892ac03323b0fefc0fe1feabc3123e084
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-09-08 00:21:14 +0900

    [add] host2_location1, host2_location2に関するテストも追加

--- test/unit/http_serverinfo_check/test_http_serverinfo_check.cpp
+++ test/unit/http_serverinfo_check/test_http_serverinfo_check.cpp
@@ -356,8 +356,8 @@ int Test1() {
 
 	server::VirtualServerAddrList virtual_servers = BuildVirtualServerAddrList();
 	CheckServerInfoResult         result   = HttpServerInfoCheck::Check(virtual_servers, request);
-	server::VirtualServer         vs       = *virtual_servers.front();     // host1
-	server::Location              location = vs.GetLocationList().front(); // location1
+	const server::VirtualServer  *vs       = virtual_servers.front();       // host1
+	server::Location              location = vs->GetLocationList().front(); // location1
 
 	try {
 		COMPARE(result.path, location.request_uri);
@@ -381,7 +381,7 @@ int Test1() {
 
 int Test2() {
 	// request
-	const RequestLine request_line = {"GET", "/www/test.html", "HTTP/1.1"}; // location2(redirect)
+	const RequestLine request_line = {"GET", "/www/test.html", "HTTP/1.1"};
 	HttpRequestFormat request;
 	request.request_line              = request_line;
 	request.header_fields[HOST]       = "host1";
@@ -389,8 +389,8 @@ int Test2() {
 
 	server::VirtualServerAddrList virtual_servers = BuildVirtualServerAddrList();
 	CheckServerInfoResult         result = HttpServerInfoCheck::Check(virtual_servers, request);
-	server::VirtualServer         vs     = *virtual_servers.front();      // host1
-	server::Location location = *(Next(vs.GetLocationList().begin(), 1)); // location2(redirect)
+	const server::VirtualServer  *vs     = virtual_servers.front();        // host1
+	server::Location location = *(Next(vs->GetLocationList().begin(), 1)); // location2(redirect)
 
 	try {
 		COMPARE(result.index, location.index);
@@ -411,67 +411,72 @@ int Test2() {
 	return EXIT_SUCCESS;
 }
 
-// int Test3() {
-// 	// request
-// 	const RequestLine request_line = {"GET", "/www/data/test.html", "HTTP/1.1"};
-// 	HttpRequestFormat request;
-// 	request.request_line              = request_line;
-// 	request.header_fields[HOST]       = "host2";
-// 	request.header_fields[CONNECTION] = "keep-alive";
+int Test3() {
+	// request
+	const RequestLine request_line = {"GET", "/www/data/test.html", "HTTP/1.1"};
+	HttpRequestFormat request;
+	request.request_line              = request_line;
+	request.header_fields[HOST]       = "host2";
+	request.header_fields[CONNECTION] = "keep-alive";
 
-// 	server::VirtualServerAddrList virtual_servers = BuildVirtualServerAddrList();
-// 	CheckServerInfoResult         result = HttpServerInfoCheck::Check(virtual_servers, request);
-// 	server::Location              location = // 二番目のサーバーにする
-// 		*(Next((*virtual_servers.begin())->GetLocationList().begin(), 1)); // location3 (alias)
+	server::VirtualServerAddrList virtual_servers = BuildVirtualServerAddrList();
+	CheckServerInfoResult         result   = HttpServerInfoCheck::Check(virtual_servers, request);
+	const server::VirtualServer  *vs       = *(Next(virtual_servers.begin(), 1)); // host2
+	server::Location              location = vs->GetLocationList().front(); // location1(alias)
 
-// 	try {
-// 		COMPARE(result.path, location.alias + "test.html");
-// 		COMPARE(result.index, location.index);
-// 		COMPARE(result.autoindex, location.autoindex);
-// 		COMPARE(result.allowed_methods, location.allowed_methods);
-// 		COMPARE(result.cgi_extension, location.cgi_extension);
-// 		COMPARE(result.upload_directory, location.upload_directory);
-// 		COMPARE(result.redirect.GetValue(), location.redirect);
-// 		COMPARE(result.error_page.GetValue(), virtual_servers.front()->GetErrorPage());
-// 	} catch (const std::exception &e) {
-// 		PrintNg();
-// 		std::cerr << e.what() << '\n';
-// 		return EXIT_FAILURE;
-// 	}
-// 	PrintOk();
-// 	return EXIT_SUCCESS;
-// }
+	try {
+		COMPARE(result.path, location.alias + "test.html");
+		COMPARE(result.index, location.index);
+		COMPARE(result.autoindex, location.autoindex);
+		COMPARE(result.allowed_methods, location.allowed_methods);
+		COMPARE(result.cgi_extension, location.cgi_extension);
+		COMPARE(result.upload_directory, location.upload_directory);
+		COMPARE(result.redirect.GetValue(), location.redirect);
+		COMPARE(result.error_page.GetValue(), virtual_servers.front()->GetErrorPage());
+	} catch (const std::exception &e) {
+		PrintNg();
+		std::cerr << e.what() << '\n';
+		DeleteAddrList(virtual_servers);
+		return EXIT_FAILURE;
+	}
+	PrintOk();
+	DeleteAddrList(virtual_servers);
+	return EXIT_SUCCESS;
+}
 
-// int Test4() {
-// 	// request
-// 	const RequestLine request_line = {"GET", "/web/", "HTTP/1.1"};
-// 	HttpRequestFormat request;
-// 	request.request_line              = request_line;
-// 	request.header_fields[HOST]       = "host2";
-// 	request.header_fields[CONNECTION] = "keep-alive";
+int Test4() {
+	// request
+	const RequestLine request_line = {"GET", "/web/", "HTTP/1.1"};
+	HttpRequestFormat request;
+	request.request_line              = request_line;
+	request.header_fields[HOST]       = "host2";
+	request.header_fields[CONNECTION] = "keep-alive";
 
-// 	MockDtoServerInfos    server_info = BuildMockDtoServerInfos();
-// 	CheckServerInfoResult result      = HttpServerInfoCheck::Check(server_info, request);
-// 	MockLocationCon       location =
-// 		*(Next(server_info.locations.begin(), 3)); // location4 (cgi, upload_directory)
+	server::VirtualServerAddrList virtual_servers = BuildVirtualServerAddrList();
+	CheckServerInfoResult         result = HttpServerInfoCheck::Check(virtual_servers, request);
+	const server::VirtualServer  *vs     = *(Next(virtual_servers.begin(), 1)); // host2
+	server::Location              location =
+		*(Next(vs->GetLocationList().begin(), 1)); // location2(cgi, upload_directory)
 
-// 	try {
-// 		COMPARE(result.path, location.request_uri);
-// 		COMPARE(result.index, location.index);
-// 		COMPARE(result.autoindex, location.autoindex);
-// 		COMPARE(result.allowed_methods, location.allowed_methods);
-// 		COMPARE(result.cgi_extension, location.cgi_extension);
-// 		COMPARE(result.upload_directory, location.upload_directory);
-// 		COMPARE(result.redirect.GetValue(), location.redirect);
-// 		COMPARE(result.error_page.GetValue(), server_info.error_page);
-// 	} catch (const std::exception &e) {
-// 		PrintNg();
-// 		std::cerr << e.what() << '\n';
-// 		return EXIT_FAILURE;
-// 	}
-// 	PrintOk();
-// 	return EXIT_SUCCESS;
-// }
+	try {
+		COMPARE(result.path, location.request_uri);
+		COMPARE(result.index, location.index);
+		COMPARE(result.autoindex, location.autoindex);
+		COMPARE(result.allowed_methods, location.allowed_methods);
+		COMPARE(result.cgi_extension, location.cgi_extension);
+		COMPARE(result.upload_directory, location.upload_directory);
+		COMPARE(result.redirect.GetValue(), location.redirect);
+		COMPARE(result.error_page.GetValue(), virtual_servers.front()->GetErrorPage());
+	} catch (const std::exception &e) {
+		PrintNg();
+		std::cerr << e.what() << '\n';
+		DeleteAddrList(virtual_servers);
+		return EXIT_FAILURE;
+	}
+	PrintOk();
+	DeleteAddrList(virtual_servers);
+	return EXIT_SUCCESS;
+}
 
 } // namespace
 
@@ -484,5 +489,7 @@ int main() {
 	ret |= Test4_();
 	ret |= Test1();
 	ret |= Test2();
+	ret |= Test3();
+	ret |= Test4();
 	return ret;
 }
