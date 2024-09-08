c1d1a36ac70db7438f66d21c322c6e56b75ce49f
sawamura-hayato <sunzecun@gmail.com>
2024-09-08 14:51:00 +0900

    Add: Check if the method is GET or POST and is in AllowMethods

--- srcs/http/response/http_method.hpp
+++ srcs/http/response/http_method.hpp
@@ -26,6 +26,8 @@ class Method {
 					 std::string        &response_body_message,
 					 HeaderFields       &header_fields
 				 );
+	static bool
+	IsAllowedMethod(const std::string &method, const std::list<std::string> &allow_methods);
 
   private:
 	static StatusCode
@@ -39,9 +41,7 @@ class Method {
 	static StatusCode DeleteHandler(
 		const std::string &path, std::string &response_body_message, HeaderFields &header_fields
 	);
-	static Stat TryStat(const std::string &path);
-	static bool
-	IsAllowedMethod(const std::string &method, const std::list<std::string> &allow_methods);
+	static Stat       TryStat(const std::string &path);
 	static void       SystemExceptionHandler(const utils::SystemException &e);
 	static StatusCode FileCreationHandler(
 		const std::string &path,
