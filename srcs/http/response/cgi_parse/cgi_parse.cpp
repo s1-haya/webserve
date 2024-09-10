73b011f4430c10fbbe73ce669d13dee49f7ae5ea
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-09-10 14:12:58 +0900

    [update] 定数をグローバル変数で定義

--- srcs/http/response/cgi_parse/cgi_parse.cpp
+++ srcs/http/response/cgi_parse/cgi_parse.cpp
@@ -29,6 +29,24 @@ std::string TranslateToHtmlPath(const std::string &request_target) {
 
 } // namespace
 
+const std::string AUTH_TYPE         = "AUTH_TYPE";
+const std::string CONTENT_LENGTH    = "CONTENT_LENGTH";
+const std::string CONTENT_TYPE      = "CONTENT_TYPE";
+const std::string GATEWAY_INTERFACE = "GATEWAY_INTERFACE";
+const std::string PATH_INFO         = "PATH_INFO";
+const std::string PATH_TRANSLATED   = "PATH_TRANSLATED";
+const std::string QUERY_STRING      = "QUERY_STRING";
+const std::string REMOTE_ADDR       = "REMOTE_ADDR";
+const std::string REMOTE_HOST       = "REMOTE_HOST";
+const std::string REMOTE_IDENT      = "REMOTE_IDENT";
+const std::string REMOTE_USER       = "REMOTE_USER";
+const std::string REQUEST_METHOD    = "REQUEST_METHOD";
+const std::string SCRIPT_NAME       = "SCRIPT_NAME";
+const std::string SERVER_NAME       = "SERVER_NAME";
+const std::string SERVER_PORT       = "SERVER_PORT";
+const std::string SERVER_PROTOCOL   = "SERVER_PROTOCOL";
+const std::string SERVER_SOFTWARE   = "SERVER_SOFTWARE";
+
 MetaMap CgiParse::CreateRequestMetaVariables(
 	const HttpRequestFormat &request,
 	const std::string       &cgi_script,
@@ -36,24 +54,24 @@ MetaMap CgiParse::CreateRequestMetaVariables(
 	const std::string       &server_port
 ) {
 	MetaMap request_meta_variables;
-	request_meta_variables["AUTH_TYPE"]         = "";
-	request_meta_variables["CONTENT_LENGTH"]    = request.header_fields.at("Content-Length");
-	request_meta_variables["CONTENT_TYPE"]      = request.header_fields.at("Content-Type");
-	request_meta_variables["GATEWAY_INTERFACE"] = "CGI/1.1";
-	request_meta_variables["PATH_INFO"]         = CreatePathInfo(cgi_extension, cgi_script);
-	request_meta_variables["PATH_TRANSLATED"] =
+	request_meta_variables[AUTH_TYPE]         = "";
+	request_meta_variables[CONTENT_LENGTH]    = request.header_fields.at("Content-Length");
+	request_meta_variables[CONTENT_TYPE]      = request.header_fields.at("Content-Type");
+	request_meta_variables[GATEWAY_INTERFACE] = "CGI/1.1";
+	request_meta_variables[PATH_INFO]         = CreatePathInfo(cgi_extension, cgi_script);
+	request_meta_variables[PATH_TRANSLATED] =
 		TranslateToHtmlPath(request_meta_variables["PATH_INFO"]);
-	request_meta_variables["QUERY_STRING"]    = "";
-	request_meta_variables["REMOTE_ADDR"]     = ""; // 追加する？
-	request_meta_variables["REMOTE_HOST"]     = "";
-	request_meta_variables["REMOTE_IDENT"]    = "";
-	request_meta_variables["REMOTE_USER"]     = "";
-	request_meta_variables["REQUEST_METHOD"]  = request.request_line.method;
-	request_meta_variables["SCRIPT_NAME"]     = TranslateToCgiPath(cgi_extension, cgi_script);
-	request_meta_variables["SERVER_NAME"]     = request.header_fields.at("Host");
-	request_meta_variables["SERVER_PORT"]     = server_port;
-	request_meta_variables["SERVER_PROTOCOL"] = request.request_line.version;
-	request_meta_variables["SERVER_SOFTWARE"] = "Webserv/1.1";
+	request_meta_variables[QUERY_STRING]    = "";
+	request_meta_variables[REMOTE_ADDR]     = ""; // 追加する？
+	request_meta_variables[REMOTE_HOST]     = "";
+	request_meta_variables[REMOTE_IDENT]    = "";
+	request_meta_variables[REMOTE_USER]     = "";
+	request_meta_variables[REQUEST_METHOD]  = request.request_line.method;
+	request_meta_variables[SCRIPT_NAME]     = TranslateToCgiPath(cgi_extension, cgi_script);
+	request_meta_variables[SERVER_NAME]     = request.header_fields.at("Host");
+	request_meta_variables[SERVER_PORT]     = server_port;
+	request_meta_variables[SERVER_PROTOCOL] = request.request_line.version;
+	request_meta_variables[SERVER_SOFTWARE] = "Webserv/1.1";
 	return request_meta_variables;
 }
 
--- srcs/http/response/cgi_parse/cgi_parse.hpp
+++ srcs/http/response/cgi_parse/cgi_parse.hpp
@@ -8,6 +8,25 @@ namespace cgi {
 
 typedef std::map<std::string, std::string> MetaMap;
 
+// namespace cgi用
+extern const std::string AUTH_TYPE;
+extern const std::string CONTENT_LENGTH;
+extern const std::string CONTENT_TYPE;
+extern const std::string GATEWAY_INTERFACE;
+extern const std::string PATH_INFO;
+extern const std::string PATH_TRANSLATED;
+extern const std::string QUERY_STRING;
+extern const std::string REMOTE_ADDR;
+extern const std::string REMOTE_HOST;
+extern const std::string REMOTE_IDENT;
+extern const std::string REMOTE_USER;
+extern const std::string REQUEST_METHOD;
+extern const std::string SCRIPT_NAME;
+extern const std::string SERVER_NAME;
+extern const std::string SERVER_PORT;
+extern const std::string SERVER_PROTOCOL;
+extern const std::string SERVER_SOFTWARE;
+
 struct CgiRequest {
 	MetaMap     meta_variables;
 	std::string body_message;
