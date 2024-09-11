ed72dfa9cee19c6408ccdb3f5248e668674280ac
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-09-11 15:29:53 +0900

    [add] ErrorHandling用のsystemcallラッパー関数を作成

--- srcs/http/response/cgi/cgi.cpp
+++ srcs/http/response/cgi/cgi.cpp
@@ -12,6 +12,67 @@
 
 namespace http {
 namespace cgi {
+namespace {
+
+static const int SYSTEM_ERROR = -1;
+
+int Close(int fd) {
+	int status = close(fd);
+	if (status == SYSTEM_ERROR) {
+		throw utils::SystemException(std::strerror(errno), errno);
+	}
+	return status;
+}
+
+int Dup2(int fd1, int fd2) {
+	int status = dup2(fd1, fd2);
+	if (status == SYSTEM_ERROR) {
+		throw utils::SystemException(std::strerror(errno), errno);
+	}
+	return status;
+}
+
+int Pipe(int fd[2]) {
+	int status = pipe(fd);
+	if (status == SYSTEM_ERROR) {
+		throw utils::SystemException(std::strerror(errno), errno);
+	}
+	return status;
+}
+
+pid_t Fork(void) {
+	pid_t p = fork();
+	if (p == SYSTEM_ERROR) {
+		throw utils::SystemException(std::strerror(errno), errno);
+	}
+	return p;
+}
+
+ssize_t Write(int fd, const void *buf, size_t nbyte) {
+	ssize_t bytes_write = write(fd, buf, nbyte);
+	if (bytes_write == SYSTEM_ERROR) {
+		throw utils::SystemException(std::strerror(errno), errno);
+	}
+	return bytes_write;
+}
+
+ssize_t Read(int fd, void *buf, size_t nbyte) {
+	ssize_t bytes_read = read(fd, buf, nbyte);
+	if (bytes_read == SYSTEM_ERROR) {
+		throw utils::SystemException(std::strerror(errno), errno);
+	}
+	return bytes_read;
+}
+
+pid_t Waitpid(pid_t pid, int *stat_loc, int options) {
+	pid_t p = waitpid(pid, stat_loc, options);
+	if (p == SYSTEM_ERROR) {
+		throw utils::SystemException(std::strerror(errno), errno);
+	}
+	return p;
+}
+
+} // namespace
 
 // 他のところでチェックしてここのatではthrowされない様にする
 Cgi::Cgi(const CgiRequest &request)
@@ -40,39 +101,35 @@ void Cgi::Execve() {
 	int cgi_request[2];
 	int cgi_response[2];
 
-	if (method_ == POST && pipe(cgi_request) == SYSTEM_ERROR) {
-		throw utils::SystemException(std::strerror(errno), errno);
+	if (method_ == POST) {
+		Pipe(cgi_request);
 	}
-	if (pipe(cgi_response) == SYSTEM_ERROR) {
-		throw utils::SystemException(std::strerror(errno), errno);
-	}
-	pid_t p = fork();
-	if (p == SYSTEM_ERROR) {
-		throw utils::SystemException(std::strerror(errno), errno);
-	} else if (p == 0) {
+	Pipe(cgi_response);
+	pid_t p = Fork();
+	if (p == 0) {
 		if (method_ == POST) {
-			close(cgi_request[WRITE]);
-			dup2(cgi_request[READ], STDIN_FILENO);
-			close(cgi_request[READ]);
+			Close(cgi_request[WRITE]);
+			Dup2(cgi_request[READ], STDIN_FILENO);
+			Close(cgi_request[READ]);
 		}
-		close(cgi_response[READ]);
-		dup2(cgi_response[WRITE], STDOUT_FILENO);
-		close(cgi_response[WRITE]);
+		Close(cgi_response[READ]);
+		Dup2(cgi_response[WRITE], STDOUT_FILENO);
+		Close(cgi_response[WRITE]);
 		ExecveCgiScript();
 	}
 	if (method_ == POST) {
-		close(cgi_request[READ]);
-		write(cgi_request[WRITE], request_body_message_.c_str(), request_body_message_.length());
-		close(cgi_request[WRITE]);
+		Close(cgi_request[READ]);
+		Write(cgi_request[WRITE], request_body_message_.c_str(), request_body_message_.length());
+		Close(cgi_request[WRITE]);
 	}
-	close(cgi_response[WRITE]);
+	Close(cgi_response[WRITE]);
 	char    buffer[1024]; // 読み取りバッファ
 	ssize_t bytes_read;
-	while ((bytes_read = read(cgi_response[READ], buffer, sizeof(buffer))) > 0) {
+	while ((bytes_read = Read(cgi_response[READ], buffer, sizeof(buffer))) > 0) {
 		response_body_message_.append(buffer, bytes_read);
 	}
-	close(cgi_response[READ]);
-	waitpid(p, &exit_status_, WNOHANG);
+	Close(cgi_response[READ]);
+	Waitpid(p, &exit_status_, WNOHANG);
 }
 
 void Cgi::Free() {
@@ -115,8 +172,9 @@ char *const *Cgi::SetCgiEnv(const MetaMap &meta_variables) {
 		const std::string element = it->first + "=" + it->second;
 		char             *dest    = new char[element.size() + 1];
 		// todo error(new(std::nothrow))
-		if (dest == NULL)
+		if (dest == NULL) {
 			return NULL;
+		}
 		std::strcpy(dest, element.c_str());
 		cgi_env[i] = dest;
 		i++;
