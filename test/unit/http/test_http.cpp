ab1541ffddb693551ab0a0722e03a4a3f4f622c6
sawamura-hayato <sunzecun@gmail.com>
2024-09-13 09:23:40 +0900

    Add: Httpテストの構想作成

--- test/unit/http/test_http.cpp
+++ test/unit/http/test_http.cpp
@@ -1,49 +1,48 @@
 #include "http.hpp"
 
+/**
+ * @brief Test Http class: HttpResultの返り値を確認するテスト。
+ *
+ * テスト一覧（今後追加）
+ * - メソッドが機能しているかどうか
+ * 	- GET
+ * 	- POST
+ * 	- DELETE
+ * - リクエストターゲットが機能しているかどうか(CGIは今後)
+ * 	- 存在してるファイル
+ * 	- 存在してないファイル
+ * 	- 存在してるディレクトリ（末尾に/ありとなし）
+ * 	- 存在してないディレクトリ（末尾に/ありとなし）
+ * - ヘッダーフィールドごとに機能しているかどうか(今後、追加)
+ * 	- Host
+ * 	- Content-Length
+ * - VirtualServer, Locationの各ディレクティブが機能しているかどうか
+ * 	- VirtualServer
+ * 	 - server_names
+ * 	- Location
+ * 	 - allow_methods
+ *
+ * responseの期待値はstatus_line, header_fields, CRLF, body_messageを組み合わせる。
+ * status_lineとデフォルトのステータスコードを返すbody_messageはファイルで用意する。header_fieldsは可変のため文字列で作成する。
+ * response以外のHttpResultのパラメータの期待値はテストごとに作成。
+ *
+ * HttpResult
+ * - is_response_complete レスポンスの書式が完全かどうか
+ * - is_connection_keep   次の接続が可能かどうか
+ * - request_buf          バッファ
+ * - response             文字列
+ *
+ * Locationのディレクティブを網羅するテストができるようなvirtual_serversを設定する。
+ * client_infosはfdは1で固定する。request_bufはファイルから読み込むで文字列にする。
+ *
+ * @param client_infos
+ * @param virtual_servers
+ *
+ * @return HttpResult indicating whether the response is complete
+ *         and containing the response data.
+ */
+
 int main(void) {
 	int ret_code = 0;
-	// // 10.ボディメッセージを追加で送信した場合
-	// http::Http                  test4_body_message;
-	// http::HttpRequestParsedData test4_expected_body_message;
-	// test4_expected_body_message.request_result.status_code          = http::StatusCode(http::OK);
-	// test4_expected_body_message.is_request_format.is_request_line   = true;
-	// test4_expected_body_message.is_request_format.is_header_fields  = true;
-	// test4_expected_body_message.is_request_format.is_body_message   = false;
-	// test4_expected_body_message.request_result.request.body_message = "abc";
-
-	// test4_body_message.ParseHttpRequestFormat(
-	// 	1, "GET / HTTP/1.1\r\nHost: test\r\nContent-Length:  3\r\n\r\na"
-	// );
-	// Result result = IsSameHttpRequestParsedData(
-	// 	test4_body_message.GetClientData(1), test4_expected_body_message
-	// );
-	// ret_code |= HandleResult(result);
-
-	// // 11.追加でボディメッセージを送信
-	// test4_body_message.ParseHttpRequestFormat(1, "bc");
-	// test4_expected_body_message.is_request_format.is_body_message = true;
-	// result                                                        = IsSameHttpRequestParsedData(
-	//     test4_body_message.GetClientData(1), test4_expected_body_message
-	// );
-	// ret_code |= HandleResult(result);
-
-	// todo: http/http_response/test_http_response.cpp responseを確認、実行、作成のテスト
-	// リクエストのステータスコードが200の場合
-	// http::Http test1_response;
-	// test1_response.ParseHttpRequestFormat(
-	// 	1, "GET / HTTP/1.1\r\nHost: test\r\nContent-Length:  3\r\n\r\na"
-	// );
-	// const std::string test1_expected_response =
-	// 	"HTTP/1.1 200 OK\r\nConnection: close\r\nHost: sawa\r\n\r\nYou can't connect the dots "
-	// 	"looking forward. You can only connect the dots looking backwards";
-	// ret_code |= HandleResult(test1_response.CreateHttpResponse(1), test1_expected_response);
-
-	// // リクエストのステータスコードが400の場合
-	// http::Http test2_response;
-	// test2_response.ParseHttpRequestFormat(
-	// 	1, "GET / HTTP/1.\r\nHost test\r\nContent-Length:  3\r\n\r\na"
-	// );
-	// std::string test2_expected_response = LoadFileContent("expected_response/test2.txt");
-	// ret_code |= HandleResult(test2_response.CreateHttpResponse(1), test2_expected_response);
 	return ret_code;
 }
