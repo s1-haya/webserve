8b3fdea7f692f42e58d950e55f2f6d458d662741
sawamura-hayato <sunzecun@gmail.com>
2024-09-14 23:32:48 +0900

    Add: HandleHttpResult

--- test/unit/http/test_http.cpp
+++ test/unit/http/test_http.cpp
@@ -1,4 +1,5 @@
 #include "http.hpp"
+#include "http_result.hpp"
 
 /**
  * @brief Test Http class: HttpResultの返り値を確認するテスト。
@@ -42,7 +43,67 @@
  *         and containing the response data.
  */
 
+namespace {
+
+struct Result {
+	Result() : is_success(true) {}
+	bool        is_success;
+	std::string error_log;
+};
+
+int GetTestCaseNum() {
+	static unsigned int test_case_num = 0;
+	++test_case_num;
+	return test_case_num;
+}
+
+template <typename T>
+bool IsSame(const T &result, const T &expected) {
+	return result == expected;
+}
+
+int HandleResult(const Result &result) {
+	if (result.is_success) {
+		std::cout << utils::color::GREEN << GetTestCaseNum() << ".[OK]" << utils::color::RESET
+				  << std::endl;
+		return EXIT_SUCCESS;
+	} else {
+		std::cerr << utils::color::RED << GetTestCaseNum() << ".[NG] " << utils::color::RESET
+				  << std::endl;
+		std::cerr << result.error_log;
+		return EXIT_FAILURE;
+	}
+}
+
+Result IsSameHttpResult(const http::HttpResult &http_result, const http::HttpResult expected) {
+	Result result;
+	// - IsSameIsResponseComplete
+	//	input bool is_response_complete
+	//	output Result
+	// - IsSameIsConnectionKeep
+	// - IsSameRequestBuffer
+	// - IsSameHttpResponse
+	(void)http_result;
+	(void)expected;
+	return result;
+}
+
+int HandleHttpResult(const http::HttpResult &http_result, const http::HttpResult expected) {
+	const Result &result = IsSameHttpResult(http_result, expected);
+	return HandleResult(result);
+}
+
+}
+
 int main(void) {
 	int ret_code = 0;
+
+	// CreateHttpResult
+	// input parameter in HttpResult
+	// ouput HttpResult
+	http::HttpResult result;
+	http::HttpResult expected;
+	ret_code |= HandleHttpResult(result, expected);
+
 	return ret_code;
 }
