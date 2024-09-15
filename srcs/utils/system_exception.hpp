34b6e8aa083be3c5d972aa9cc5a3e15c864dc3dd
habvi <93956895+habvi@users.noreply.github.com>
2024-09-16 03:04:33 +0900

    [add] cppcheck / (少し関係ないが)explicit修正

--- srcs/utils/system_exception.hpp
+++ srcs/utils/system_exception.hpp
@@ -8,7 +8,7 @@ namespace utils {
 
 class SystemException : public std::runtime_error {
   public:
-	explicit SystemException(const std::string &message, int error_number);
+	SystemException(const std::string &message, int error_number);
 	int GetErrorNumber() const;
 
   private:
