27edf2b9f5575ff0f497fc36769b270d22d30c22
sawamura-hayato <sunzecun@gmail.com>
2024-09-16 18:52:54 +0900

    style format

--- test/webserv/unit/http/test_case/test_case.hpp
+++ test/webserv/unit/http/test_case/test_case.hpp
@@ -15,6 +15,6 @@ namespace test {
 int test_no_connection(const server::VirtualServerAddrList &server_infos);
 int test_not_exist_file(const server::VirtualServerAddrList &server_infos);
 
-}
+} // namespace test
 
 #endif
