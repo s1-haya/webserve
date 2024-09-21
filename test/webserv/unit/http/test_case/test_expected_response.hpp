d607dc747404243145c56f239ef826a9cfdee3b8
sawamura-hayato <sunzecun@gmail.com>
2024-09-21 12:59:40 +0900

    Fix: 参照渡し

--- test/webserv/unit/http/test_case/test_expected_response.hpp
+++ test/webserv/unit/http/test_case/test_expected_response.hpp
@@ -4,16 +4,16 @@ namespace test {
 
 std::string LoadFileContent(const std::string &file_path);
 
-static const std::string EXPECTED_RESPONSE     = "../../expected_response";
+static const std::string &EXPECTED_RESPONSE     = "../../expected_response";
 
 // status_line
-static const std::string EXPECTED_STATUS_LINE  = EXPECTED_RESPONSE + "/default_status_line";
-static const std::string EXPECTED_STATUS_LINE_OK =
+static const std::string &EXPECTED_STATUS_LINE  = EXPECTED_RESPONSE + "/default_status_line";
+static const std::string &EXPECTED_STATUS_LINE_OK =
 	LoadFileContent(EXPECTED_STATUS_LINE + "/ok.txt");
-static const std::string EXPECTED_STATUS_LINE_NOT_FOUND = LoadFileContent(EXPECTED_STATUS_LINE + "/not_found.txt");
+static const std::string &EXPECTED_STATUS_LINE_NOT_FOUND = LoadFileContent(EXPECTED_STATUS_LINE + "/not_found.txt");
 
 // body_message
-static const std::string EXPECTED_BODY_MESSAGE = EXPECTED_RESPONSE + "/default_body_message";
-static const std::string EXPECTED_BODY_MESSAGE_NOT_FOUND =
+static const std::string &EXPECTED_BODY_MESSAGE = EXPECTED_RESPONSE + "/default_body_message";
+static const std::string &EXPECTED_BODY_MESSAGE_NOT_FOUND =
 	LoadFileContent(EXPECTED_BODY_MESSAGE + "/not_found.txt");
 }
