372bdfda70dc1949ceb3897d9e45b281626762e8
habvi <93956895+habvi@users.noreply.github.com>
2024-09-26 19:18:47 +0900

    [add] cgi_manager / logic_error追加

--- srcs/server/cgi_manager/cgi_manager.cpp
+++ srcs/server/cgi_manager/cgi_manager.cpp
@@ -1,4 +1,5 @@
 #include "cgi_manager.hpp"
+#include <stdexcept> // logic_error
 
 namespace server {
 
@@ -14,7 +15,10 @@ CgiManager::~CgiManager() {
 void CgiManager::AddNewCgi(int client_fd, const cgi::CgiRequest &request) {
 	// todo: new error handling
 	Cgi *cgi = new Cgi(request);
-	// todo: add logic_error?
+
+	if (cgi_addr_map_.count(client_fd) != 0) {
+		throw std::logic_error("AddNewCgi: client_fd already exists");
+	}
 	cgi_addr_map_[client_fd] = cgi;
 }
 
@@ -27,7 +31,6 @@ CgiManager::Cgi::CgiResult CgiManager::RunCgi(int client_fd) {
 		return cgi_result;
 	}
 	// pipe_fdとclient_fdの紐づけをFdMapに追加
-	// todo: add logic_error?
 	if (cgi->IsReadRequired()) {
 		client_fd_map_[cgi->GetReadFd()] = client_fd;
 	}
@@ -54,19 +57,30 @@ void CgiManager::DeleteCgi(int client_fd) {
 }
 
 bool CgiManager::IsResponseComplete(int client_fd) const {
-	const Cgi *cgi = cgi_addr_map_.at(client_fd);
-	return cgi->IsResponseComplete();
+	try {
+		const Cgi *cgi = cgi_addr_map_.at(client_fd);
+		return cgi->IsResponseComplete();
+	} catch (const std::exception &e) {
+		throw std::logic_error("IsResponseComplete: " + std::string(e.what()));
+	}
 }
 
 void CgiManager::AddReadBuf(int client_fd, const std::string &read_buf) {
-	Cgi *cgi = cgi_addr_map_.at(client_fd);
-	cgi->AddReadBuf(read_buf);
+	try {
+		Cgi *cgi = cgi_addr_map_.at(client_fd);
+		cgi->AddReadBuf(read_buf);
+	} catch (const std::exception &e) {
+		throw std::logic_error("AddReadBuf: " + std::string(e.what()));
+	}
 }
 
 CgiManager::GetFdResult CgiManager::GetReadFd(int client_fd) const {
-	GetFdResult result;
+	if (cgi_addr_map_.count(client_fd) == 0) {
+		throw std::logic_error("GetReadFd: client_fd doesn't exists");
+	}
 
-	const Cgi *cgi = cgi_addr_map_.at(client_fd);
+	GetFdResult result;
+	const Cgi  *cgi = cgi_addr_map_.at(client_fd);
 	if (!cgi->IsReadRequired()) {
 		result.Set(false);
 		return result;
@@ -76,9 +90,12 @@ CgiManager::GetFdResult CgiManager::GetReadFd(int client_fd) const {
 }
 
 CgiManager::GetFdResult CgiManager::GetWriteFd(int client_fd) const {
-	GetFdResult result;
+	if (cgi_addr_map_.count(client_fd) == 0) {
+		throw std::logic_error("GetWriteFd: client_fd doesn't exists");
+	}
 
-	const Cgi *cgi = cgi_addr_map_.at(client_fd);
+	GetFdResult result;
+	const Cgi  *cgi = cgi_addr_map_.at(client_fd);
 	if (!cgi->IsWriteRequired()) {
 		result.Set(false);
 		return result;
@@ -88,34 +105,55 @@ CgiManager::GetFdResult CgiManager::GetWriteFd(int client_fd) const {
 }
 
 int CgiManager::GetClientFd(int pipe_fd) const {
-	// todo: add logic_error?
-	return client_fd_map_.at(pipe_fd);
+	try {
+		return client_fd_map_.at(pipe_fd);
+	} catch (const std::exception &e) {
+		throw std::logic_error("GetClientFd: " + std::string(e.what()));
+	}
 }
 
 const std::string &CgiManager::GetRequest(int client_fd) const {
-	const Cgi *cgi = cgi_addr_map_.at(client_fd);
-	return cgi->GetRequest();
+	try {
+		const Cgi *cgi = cgi_addr_map_.at(client_fd);
+		return cgi->GetRequest();
+	} catch (const std::exception &e) {
+		throw std::logic_error("GetRequest: " + std::string(e.what()));
+	}
 }
 
 // todo: 返り値がcgi::CgiResponseになりそう
 const std::string &CgiManager::GetResponse(int client_fd) const {
-	const Cgi *cgi = cgi_addr_map_.at(client_fd);
-	return cgi->GetResponse();
+	try {
+		const Cgi *cgi = cgi_addr_map_.at(client_fd);
+		return cgi->GetResponse();
+	} catch (const std::exception &e) {
+		throw std::logic_error("GetResponse: " + std::string(e.what()));
+	}
 }
 
 void CgiManager::ReplaceNewRequest(int client_fd, const std::string &new_request_str) {
-	Cgi *cgi = cgi_addr_map_.at(client_fd);
-	cgi->ReplaceNewRequest(new_request_str);
+	try {
+		Cgi *cgi = cgi_addr_map_.at(client_fd);
+		cgi->ReplaceNewRequest(new_request_str);
+	} catch (const std::exception &e) {
+		throw std::logic_error("ReplaceNewRequest: " + std::string(e.what()));
+	}
 }
 
 CgiManager::Cgi *CgiManager::GetCgi(int client_fd) {
-	// todo: add logic_error?
-	return cgi_addr_map_.at(client_fd);
+	try {
+		return cgi_addr_map_.at(client_fd);
+	} catch (const std::exception &e) {
+		throw std::logic_error("GetCgi: " + std::string(e.what()));
+	}
 }
 
 const CgiManager::Cgi *CgiManager::GetCgi(int client_fd) const {
-	// todo: add logic_error?
-	return cgi_addr_map_.at(client_fd);
+	try {
+		return cgi_addr_map_.at(client_fd);
+	} catch (const std::exception &e) {
+		throw std::logic_error("GetCgi: " + std::string(e.what()));
+	}
 }
 
 } // namespace server
