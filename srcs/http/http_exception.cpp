623b402cd28c652096a8876b8fbb8712c73ed10b
sawamura-hayato <sunzecun@gmail.com>
2024-09-27 14:27:29 +0900

    Update: HttpExceptionにDebug用の文字列追加

--- srcs/http/http_exception.cpp
+++ srcs/http/http_exception.cpp
@@ -1,9 +1,13 @@
 #include "http_exception.hpp"
+#include <iostream>
+#include "utils.hpp"
 
 namespace http {
 
 HttpException::HttpException(const std::string &error_message, const StatusCode &status_code)
-	: runtime_error(error_message), status_code_(status_code) {}
+	: runtime_error(error_message), status_code_(status_code) {
+		std::cerr << utils::color::GRAY << "[HttpException] " << error_message << utils::color::RESET << std::endl;
+}
 
 HttpException::~HttpException() throw() {}
 
