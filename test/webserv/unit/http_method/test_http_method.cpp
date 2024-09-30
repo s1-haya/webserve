1e184712fb35ecda3b3a939dfa36293b1d20cf8c
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-09-30 12:34:33 +0900

    [fix] Testが通るようにHttpMethodTestのパス全般を変更

--- test/webserv/unit/http_method/test_http_method.cpp
+++ test/webserv/unit/http_method/test_http_method.cpp
@@ -167,9 +167,9 @@ int main(void) {
 
 	// http_method/expected
 	// LF:   exist target resourse file
-	std::string expected_file       = LoadFileContent("test/file.txt");
-	std::string expected_index_file = LoadFileContent("test/index.txt");
-	std::string expected_autoindex  = CreateAutoIndexContent("test/");
+	std::string expected_file       = LoadFileContent("root/file.txt");
+	std::string expected_index_file = LoadFileContent("root/index.txt");
+	std::string expected_autoindex  = CreateAutoIndexContent("root/");
 	// CRLF: use default status code file
 	std::string expected_created =
 		LoadFileContent("../../expected_response/default_body_message/201_created.txt");
@@ -186,7 +186,7 @@ int main(void) {
 	// ファイルが存在する場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/file.txt", http::GET, allow_methods, request, response, response_header_fields
+			"root/file.txt", http::GET, allow_methods, request, response, response_header_fields
 		),
 		expected_file
 	);
@@ -194,7 +194,7 @@ int main(void) {
 	// ファイルが存在しない場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/a", http::GET, allow_methods, request, response, response_header_fields
+			"root/a", http::GET, allow_methods, request, response, response_header_fields
 		),
 		expected_not_found
 	);
@@ -202,7 +202,7 @@ int main(void) {
 	// ディレクトリの場合かつ'/'がない場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/directory", http::GET, allow_methods, request, response, response_header_fields
+			"root/directory", http::GET, allow_methods, request, response, response_header_fields
 		),
 		expected_redirect
 	);
@@ -210,7 +210,7 @@ int main(void) {
 	// ファイルが権限ない場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/no_authority_file",
+			"root/no_authority_file",
 			http::GET,
 			allow_methods,
 			request,
@@ -223,7 +223,7 @@ int main(void) {
 	// ディレクトリで'/'があり、indexがある場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/",
+			"root/",
 			http::GET,
 			allow_methods,
 			request,
@@ -237,7 +237,7 @@ int main(void) {
 	// ディレクトリで'/'があり、autoindexがある場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/", http::GET, allow_methods, request, response, response_header_fields, "", true
+			"root/", http::GET, allow_methods, request, response, response_header_fields, "", true
 		),
 		expected_autoindex
 	);
@@ -247,7 +247,7 @@ int main(void) {
 	const std::string &post_test1_request_body_message = "OK";
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"./root/upload/200_ok.txt",
+			"root/directory/200_ok.txt",
 			http::POST,
 			allow_methods,
 			post_test1_request_body_message,
@@ -255,7 +255,7 @@ int main(void) {
 			response_header_fields,
 			"",
 			false,
-			"/upload"
+			"/directory"
 		),
 		expected_created
 	);
@@ -264,7 +264,7 @@ int main(void) {
 	const std::string &post_test2_request_body_message = "OK";
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"./root/upload/200_ok.txt",
+			"root/directory/200_ok.txt",
 			http::POST,
 			allow_methods,
 			post_test2_request_body_message,
@@ -272,7 +272,7 @@ int main(void) {
 			response_header_fields,
 			"",
 			false,
-			"/upload"
+			"/directory"
 		),
 		expected_no_content
 	);
@@ -281,7 +281,7 @@ int main(void) {
 	const std::string &post_test3_request_body_message = "OK";
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"./root/200_ok.txt",
+			"root/200_ok.txt",
 			http::POST,
 			allow_methods,
 			post_test3_request_body_message,
@@ -289,7 +289,7 @@ int main(void) {
 			response_header_fields,
 			"",
 			false,
-			"/save"
+			"/upload"
 		),
 		expected_created
 	);
@@ -297,7 +297,7 @@ int main(void) {
 	// ディレクトリの場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/directory", http::POST, allow_methods, request, response, response_header_fields
+			"root/directory/", http::POST, allow_methods, request, response, response_header_fields
 		),
 		expected_forbidden
 	);
@@ -306,7 +306,12 @@ int main(void) {
 	// ファイルが存在するかつ親ディレクトリが書き込み権限あるとき
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"200_ok.txt", http::DELETE, allow_methods, request, response, response_header_fields
+			"root/directory/200_ok.txt",
+			http::DELETE,
+			allow_methods,
+			request,
+			response,
+			response_header_fields
 		),
 		expected_no_content
 	);
@@ -335,7 +340,7 @@ int main(void) {
 	// ディレクトリ内にファイルが存在してない場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/s", http::DELETE, allow_methods, request, response, response_header_fields
+			"root/s", http::DELETE, allow_methods, request, response, response_header_fields
 		),
 		expected_forbidden
 	);
@@ -356,7 +361,7 @@ int main(void) {
 	// 書き込み権限がないディレクトリの中にあるファイル場合
 	ret_code |= MethodHandlerResult(
 		MethodArgument(
-			"test/no_authority_directory/test.txt",
+			"root/no_authority_directory/test.txt",
 			http::DELETE,
 			allow_methods,
 			request,
