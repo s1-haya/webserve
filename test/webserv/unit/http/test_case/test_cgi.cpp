6a21ce51f3610be8b7310f91d3332061bbba9025
habvi <93956895+habvi@users.noreply.github.com>
2024-10-02 01:19:34 +0900

    [move] request / get/200_19をcgi/get/200_03として移動

--- test/webserv/unit/http/test_case/test_cgi.cpp
+++ test/webserv/unit/http/test_case/test_cgi.cpp
@@ -41,4 +41,20 @@ int TestCgiGetOk2PrintOkInPerl(const server::VirtualServerAddrList &server_infos
 	return HandleHttpResult(client_infos, server_infos, expected, "200-02");
 }
 
+int TestCgiGetOk3PrintOkInPython(const server::VirtualServerAddrList &server_infos) {
+	http::ClientInfos client_infos = CreateClientInfos(request::cgi::GET_200_03_PRINT_OK_PYTHON);
+	std::string       expected_status_line  = EXPECTED_STATUS_LINE_OK;
+	std::string       expected_body_message = "OK";
+	HeaderFields      expected_header_fields;
+	expected_header_fields[http::CONNECTION]     = http::CLOSE;
+	expected_header_fields[http::CONTENT_LENGTH] = utils::ToString(expected_body_message.length());
+	expected_header_fields[http::CONTENT_TYPE]   = http::TEXT_PLAIN;
+	expected_header_fields[http::SERVER]         = http::SERVER_VERSION;
+	const std::string &expected_response         = CreateHttpResponseFormat(
+        expected_status_line, expected_header_fields, expected_body_message
+    );
+	http::HttpResult expected = CreateHttpResult(true, false, "", expected_response);
+	return HandleHttpResult(client_infos, server_infos, expected, "200-03");
+}
+
 } // namespace test
--- test/webserv/unit/http/test_case/test_request.hpp
+++ test/webserv/unit/http/test_case/test_request.hpp
@@ -41,8 +41,6 @@ static const std::string &GET_200_15_BODY_MESSAGE_DEFAULT =
 	LoadFileContent(REQUEST_GET + ROOT_2XX + "/200_15_body_message_default.txt");
 static const std::string &GET_200_17_NOT_EXIST_HEADER_FIELD =
 	LoadFileContent(REQUEST_GET + ROOT_2XX + "/200_17_not_exist_header_field.txt");
-static const std::string &GET_200_19_CGI_SCRIPT_IN_PYTHON =
-	LoadFileContent(REQUEST_GET + ROOT_2XX + "/200_19_cgi_script_in_py.txt");
 static const std::string &GET_200_20_CGI_SCRIPT_IN_SHELL =
 	LoadFileContent(REQUEST_GET + ROOT_2XX + "/200_20_cgi_script_in_sh.txt");
 static const std::string &GET_200_21_NO_CONNECTION =
@@ -108,6 +106,8 @@ static const std::string &GET_200_01_PRINT_OK_PL_BODY_MESSAGE =
 	LoadFileContent(CGI_GET + "/200_01_print_ok_pl_body_message.txt");
 static const std::string &GET_200_02_PRINT_OK_PERL =
 	LoadFileContent(CGI_GET + "/200_02_print_ok_pl.txt");
+static const std::string &GET_200_03_PRINT_OK_PYTHON =
+	LoadFileContent(CGI_GET + "/200_03_print_ok_py.txt");
 
 } // namespace cgi
 } // namespace request
