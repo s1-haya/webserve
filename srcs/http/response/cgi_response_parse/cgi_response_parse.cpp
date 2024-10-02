eb43deb174f1448692753ad66e6e9aafbbcebdca
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-10-02 13:05:51 +0900

    [update] Testを踏まえて修正(以下詳細)
    - ParsedDataをpublicに
    - TrimOWS関数追加
    - ParseHeaderFieldsに渡すときにCRLFも含めて渡す

--- srcs/http/response/cgi_response_parse/cgi_response_parse.cpp
+++ srcs/http/response/cgi_response_parse/cgi_response_parse.cpp
@@ -5,6 +5,7 @@ namespace http {
 
 const std::string CgiResponseParse::CRLF              = "\r\n";
 const std::string CgiResponseParse::HEADER_FIELDS_END = CRLF + CRLF;
+const std::string CgiResponseParse::OWS               = " \t";
 
 CgiResponseParse::CgiResponseParse() {}
 
@@ -17,7 +18,7 @@ CgiResponseParse::ParsedData CgiResponseParse::Parse(const std::string &response
 	if (pos == std::string::npos) {
 		return parsed_data_;
 	}
-	std::string header = response.substr(0, pos);
+	std::string header = response.substr(0, pos + CRLF.size()); // CRLFも含めたいため
 	ParseHeaderFields(header);
 	ParseBody(response.substr(pos + HEADER_FIELDS_END.size()));
 	return parsed_data_;
@@ -39,7 +40,8 @@ void CgiResponseParse::ParseHeaderFields(const std::string &header) {
 		}
 		std::string key   = line.substr(0, colon_pos);
 		std::string value = line.substr(colon_pos + 1);
-		// todo: validation and trim
+		value             = TrimOWS(value);
+		// todo: validation(HttpParseの処理をそのまま使う)
 		parsed_data_.header_fields[key] = value;
 	}
 }
@@ -56,6 +58,24 @@ void CgiResponseParse::ParseBody(const std::string &body) {
 		parsed_data_.body = body.substr(0, content_length);
 		return;
 	}
+}
+
+std::string &CgiResponseParse::TrimOWS(std::string &s) {
+	// 先頭のスペースとタブを削除
+	std::string::iterator it = s.begin();
+	while (it != s.end() && CgiResponseParse::OWS.find(*it) != std::string::npos) {
+		++it;
+	}
+	s.erase(s.begin(), it);
+
+	// 末尾のスペースとタブを削除
+	std::string::reverse_iterator rit = s.rbegin();
+	while (rit != s.rend() && CgiResponseParse::OWS.find(*rit) != std::string::npos) {
+		++rit;
+	}
+	s.erase(rit.base(), s.end());
+
+	return s;
 }
 
 } // namespace http
--- srcs/http/response/cgi_response_parse/cgi_response_parse.hpp
+++ srcs/http/response/cgi_response_parse/cgi_response_parse.hpp
@@ -7,13 +7,13 @@
 namespace http {
 
 class CgiResponseParse {
+  public:
 	typedef std::map<std::string, std::string> HeaderFields;
 	struct ParsedData {
 		HeaderFields header_fields;
 		std::string  body;
 	};
 
-  public:
 	CgiResponseParse();
 	~CgiResponseParse();
 	ParsedData Parse(const std::string &response);
@@ -22,13 +22,15 @@ class CgiResponseParse {
 	CgiResponseParse(const CgiResponseParse &other);
 	CgiResponseParse &operator=(const CgiResponseParse &other);
 
-	void ParseHeaderFields(const std::string &header);
-	void ParseBody(const std::string &body);
+	void         ParseHeaderFields(const std::string &header);
+	void         ParseBody(const std::string &body);
+	std::string &TrimOWS(std::string &s);
 
 	ParsedData parsed_data_;
 
 	static const std::string CRLF;
 	static const std::string HEADER_FIELDS_END;
+	static const std::string OWS;
 };
 
 } // namespace http
