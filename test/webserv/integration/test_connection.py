cd8f57c7c0d3be21b9844e95f278109a211d0feb
habvi <93956895+habvi@users.noreply.github.com>
2024-10-03 03:43:46 +0900

    [add] integration / 通常のkeep-aliveを送り,200が返ることを確認し,timeoutまでに何も返ってこない(接続が切られた？)ことを確認

---
+++ test/webserv/integration/test_connection.py
@@ -0,0 +1,66 @@
+import socket
+from http.client import HTTPConnection, HTTPException
+from typing import Optional
+
+from common import (
+    assert_response,
+    response_header_get_root_200_keep,
+    root_index_file,
+)
+
+
+# serverのtimeout+αを設定する
+TIMEOUT = 4.0
+BUFFER_SIZE = 4096
+
+
+def receive_with_timeout(sock) -> Optional[str]:
+    # timeoutを設定
+    sock.settimeout(TIMEOUT)
+    try:
+        # timeout秒までに何かを受信したらその時点で返す
+        return sock.recv(BUFFER_SIZE).decode("utf-8")
+    except socket.timeout:
+        # timeout秒待っても何も送られてこなかった場合
+        return None
+
+
+# 通常のkeep-aliveを送信(各関数の説明付き)
+def test_timeout_keep_alive() -> None:
+    try:
+        con = HTTPConnection("localhost", 8080)
+        # serverに接続
+        con.connect()
+
+        # request送信
+        con.sock.send(
+            b"GET / HTTP/1.1\r\nHost: localhost\r\nConnection: keep-alive\r\n\r\n"
+        )
+
+        # serverからのresponseを読み込む
+        response = receive_with_timeout(con.sock)
+        if response is None:
+            # timeout秒経過していたらNG
+            assert False
+        else:
+            # serverが200 OKを返したことを確認
+            assert_response(
+                response, response_header_get_root_200_keep + root_index_file
+            )
+
+        # 再度responseを読み込み
+        next_response = receive_with_timeout(con.sock)
+        if next_response is None:
+            # timeout秒経過していたらNG
+            assert False
+        else:
+            # timeout秒経過前にserverから何も送られてこなかったらOK(接続が切れたという判断はできないかも？)
+            assert len(next_response) == 0
+
+    except HTTPException as e:
+        print(f"Request failed: {e}")
+        assert False
+    finally:
+        if con:
+            con.close()
+
