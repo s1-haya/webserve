53f0e44096b6a1f3775a4f882ebd29b7a024cb2e
sawamura-hayato <sunzecun@gmail.com>
2024-10-02 20:21:10 +0900

    Add: TestDelete1NotFoundNonexistentFile

--- test/webserv/unit/http/test_case/delete/test_4xx.cpp
+++ test/webserv/unit/http/test_case/delete/test_4xx.cpp
@@ -10,7 +10,6 @@ namespace test {
 
 // 4xx
 int TestDelete1ForbiddenDirectory(const server::VirtualServerAddrList &server_infos) {
-	int                ret_code  = EXIT_SUCCESS;
 	http::ClientInfos client_infos =
 		CreateClientInfos(request::DELETE_403_1_DIRECTORY);
 	std::string  expected_status_line  = EXPECTED_STATUS_LINE_FORBIDDEN;
@@ -24,11 +23,26 @@ int TestDelete1ForbiddenDirectory(const server::VirtualServerAddrList &server_in
         expected_status_line, expected_header_fields, expected_body_message
     );
 	http::HttpResult expected = CreateHttpResult(true, false, "", expected_response);
-	ret_code |= HandleHttpResult(client_infos, server_infos, expected, "403-01");
-	return ret_code;
+	return HandleHttpResult(client_infos, server_infos, expected, "403-01");
 }
 
-int TestDelete1NotFoundNonexistentFile(const server::VirtualServerAddrList &server_infos);
+int TestDelete1NotFoundNonexistentFile(const server::VirtualServerAddrList &server_infos) {
+	http::ClientInfos client_infos =
+		CreateClientInfos(request::DELETE_404_1_NONEXISTENT_FILE);
+	std::string  expected_status_line  = EXPECTED_STATUS_LINE_NOT_FOUND;
+	std::string  expected_body_message = EXPECTED_BODY_MESSAGE_NOT_FOUND;
+	HeaderFields expected_header_fields;
+	expected_header_fields[http::CONNECTION]     = http::CLOSE;
+	expected_header_fields[http::CONTENT_LENGTH] = utils::ToString(expected_body_message.length());
+	expected_header_fields[http::CONTENT_TYPE]   = http::TEXT_HTML;
+	expected_header_fields[http::SERVER]         = http::SERVER_VERSION;
+	const std::string &expected_response         = CreateHttpResponseFormat(
+        expected_status_line, expected_header_fields, expected_body_message
+    );
+	http::HttpResult expected = CreateHttpResult(true, false, "", expected_response);
+	return HandleHttpResult(client_infos, server_infos, expected, "404-01");
+}
+
 int TestDeleteMethodNotAllowed(const server::VirtualServerAddrList &server_infos);
 
 } // namespace test
