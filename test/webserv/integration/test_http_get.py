d496ad78092e59af121480b46d973000ec83725f
habvi <93956895+habvi@users.noreply.github.com>
2024-10-06 07:10:08 +0900

    [add] integration / Hostのfield valueが空の場合のテスト追加

--- test/webserv/integration/test_http_get.py
+++ test/webserv/integration/test_http_get.py
@@ -109,6 +109,10 @@ REQUEST_GET_5XX_DIR = REQUEST_DIR + "get/5xx/"
             bad_request_response,
         ),
         (
+            REQUEST_GET_4XX_DIR + "400_13_empty_host_header_value.txt",
+            bad_request_response,
+        ),
+        (
             REQUEST_GET_4XX_DIR + "400_15_space_in_header_field_name.txt",
             bad_request_response,
         ),
@@ -172,6 +176,7 @@ REQUEST_GET_5XX_DIR = REQUEST_DIR + "get/5xx/"
         "400_10_duplicate_host",
         "400_11_no_header_field_colon",
         "400_12_no_connection_name",
+        "400_13_empty_host_header_value",
         "400_15_space_in_header_field_name",
         "400_16_header_field_name_space_colon",
         "400_17_space_header_field_name",
