357fd4744a0ee54ab491e8a0a1c3821c311b1c5a
habvi <93956895+habvi@users.noreply.github.com>
2024-10-06 18:44:46 +0900

    [add] integration / 追加済みのpost/408_01をテストに追加(closeでcontent-length分が途中まで来ている)

--- test/webserv/integration/test_http_post.py
+++ test/webserv/integration/test_http_post.py
@@ -109,10 +109,16 @@ def test_post_upload_responses(
             bad_request_response,
             UPLOAD_FILE_PATH,
         ),
+        (
+            REQUEST_POST_4XX_DIR + "408_01_shortened_body_message.txt",
+            timeout_response,
+            UPLOAD_DIR + "shortened_body_message",
+        ),
     ],
     ids=[
         "400_01_duplicate_content_length",
         "400_02_transfer_encoding_and_content_length",
+        "408_01_shortened_body_message",
     ],
 )
 def test_post_4xx_responses(request_file, expected_response, upload_file_path):
