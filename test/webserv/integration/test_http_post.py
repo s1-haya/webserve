be77299d2d169019e70cb0788d027f45dc11425f
habvi <93956895+habvi@users.noreply.github.com>
2024-10-08 19:41:08 +0900

    [add] integration / post/400_08,408_05: chunkedで終端途中の0\r\nまで送られた場合を追加(次のリクエストが来る/来ないの2パターン)

--- test/webserv/integration/test_http_post.py
+++ test/webserv/integration/test_http_post.py
@@ -157,6 +157,11 @@ def test_post_upload_responses(
             CHUNKED_FILE_PATH,
         ),
         (
+            REQUEST_POST_4XX_DIR + "400_08_incomplete_chunked_body_0crlf_end.txt",
+            bad_request_response,
+            CHUNKED_FILE_PATH,
+        ),
+        (
             REQUEST_POST_4XX_DIR + "408_01_shortened_body_message.txt",
             timeout_response,
             UPLOAD_DIR + "shortened_body_message",
@@ -176,6 +181,11 @@ def test_post_upload_responses(
             timeout_response,
             CHUNKED_FILE_PATH,
         ),
+        (
+            REQUEST_POST_4XX_DIR + "408_05_incomplete_chunked_body_0crlf_end.txt",
+            timeout_response,
+            CHUNKED_FILE_PATH,
+        ),
     ],
     ids=[
         "400_01_duplicate_content_length",
@@ -185,10 +195,12 @@ def test_post_upload_responses(
         "400_05_no_content_length_and_not_chunked_transfer_encoding",
         "400_06_incomplete_chunked_body",
         "400_07_incomplete_chunked_body_0_end",
+        "400_08_incomplete_chunked_body_0crlf_end",
         "408_01_shortened_body_message",
         "408_02_no_body_message",
         "408_03_incomplete_chunked_body",
         "408_04_incomplete_chunked_body_0_end",
+        "408_05_incomplete_chunked_body_0crlf_end",
     ],
 )
 def test_post_4xx_responses(
