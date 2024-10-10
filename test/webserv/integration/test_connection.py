d87f2b29fd31caad9bb3b3dd7563369356d16c3a
habvi <93956895+habvi@users.noreply.github.com>
2024-10-10 16:11:17 +0900

    [style] integration / socket不要なテストをunittestで共通化

--- test/webserv/integration/test_connection.py
+++ test/webserv/integration/test_connection.py
@@ -1,51 +1,49 @@
+import unittest
 from http.client import HTTPConnection, HTTPException
 
 from common_functions import SERVER_PORT
 
 
 # ------------------------------------------------------------------------------
-# serverがその後も正常に動いていればOK
-def test_05_timeout_no_request_disconnect() -> None:
-    try:
-        con = HTTPConnection("localhost", SERVER_PORT)
-        con.connect()
+class TestConnection(unittest.TestCase):
+    def setUp(self):
+        # 各テストの前に実行される(unittestの機能)
+        self.con = HTTPConnection("localhost", SERVER_PORT)
 
-        # requestを何も送らない
+    def tearDown(self):
+        # 各テストの後に実行される(unittestの機能)
+        if self.con:
+            self.con.close()
 
-        # 何も送らずに接続を切断
-        con.close()
+    # serverがその後も正常に動いていればOK
+    def test_01_timeout_no_request_disconnect(self) -> None:
+        try:
+            # requestを何も送らない
 
-    except HTTPException as e:
-        print(f"Request failed: {e}")
-        raise AssertionError
-    finally:
-        if con:
-            con.close()
+            # 何も送らずに接続を切断
+            self.con.close()
 
+        except HTTPException as e:
+            print(f"Request failed: {e}")
+            raise AssertionError
 
-# serverがその後も正常に動いていればOK
-def test_08_shortened_body_message_disconnect() -> None:
-    try:
-        con = HTTPConnection("localhost", SERVER_PORT)
-        con.connect()
+    # serverがその後も正常に動いていればOK
+    def test_02_shortened_body_message_disconnect(self) -> None:
+        try:
+            # content-length分以下のbody
+            self.con.request(
+                "GET",
+                "/",
+                "abc",
+                {
+                    "Connection": "keep-alive",
+                    "Content-Length": "5",
+                },
+            )
 
-        # content-length分以下のbody
-        con.request(
-            "GET",
-            "/",
-            "abc",
-            {
-                "Connection": "keep-alive",
-                "Content-Length": "5",
-            },
-        )
+            # content-length分を送る前に接続を切る
+            self.con.close()
 
-        # content-length分を送る前に接続を切る
-        con.close()
-
-    except HTTPException as e:
-        print(f"Request failed: {e}")
-        raise AssertionError
-    finally:
-        if con:
-            con.close()
+        except HTTPException as e:
+            print(f"Request failed: {e}")
+            raise AssertionError
