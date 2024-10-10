4d3172d4e2c2857abb364f6eac48a6ac1a226d5a
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-10-10 23:26:37 +0900

    [fix] デフォルトのContent-Typeを修正したことによるテスト修正

--- test/webserv/unit/http_response/test_http_response.cpp
+++ test/webserv/unit/http_response/test_http_response.cpp
@@ -238,7 +238,9 @@ int main(void) {
 		LoadFileContent("../../expected_response/default_status_line/200_ok.txt");
 	std::string expected4_body_message  = "";
 	std::string expected4_header_fields = SetDefaultHeaderFields(
-		http::KEEP_ALIVE, utils::ToString(expected4_body_message.length()), http::TEXT_PLAIN
+		http::KEEP_ALIVE,
+		utils::ToString(expected4_body_message.length()),
+		http::APPLICATION_OCTET_STREAM
 	);
 	const std::string &expected4_response =
 		expected4_status_line + expected4_header_fields + http::CRLF + expected4_body_message;
