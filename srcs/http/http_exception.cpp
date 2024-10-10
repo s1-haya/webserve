63768e4ddddbbf72a73c277efe17c16709b2b756
habvi <93956895+habvi@users.noreply.github.com>
2024-10-11 01:03:14 +0900

    [style] http_exception / debugにstatus_codeも追加

--- srcs/http/http_exception.cpp
+++ srcs/http/http_exception.cpp
@@ -6,8 +6,8 @@ namespace http {
 
 HttpException::HttpException(const std::string &error_message, const StatusCode &status_code)
 	: runtime_error(error_message), status_code_(status_code) {
-	std::cerr << utils::color::GRAY << "[HttpException] " << error_message << utils::color::RESET
-			  << std::endl;
+	std::cerr << utils::color::GRAY << "[HttpException] " << status_code.GetEStatusCode() << ": "
+			  << error_message << utils::color::RESET << std::endl;
 }
 
 HttpException::~HttpException() throw() {}
