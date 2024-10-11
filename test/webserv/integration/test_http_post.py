dd00b04c80419871faa0438dad63d1f76ab3df9e
habvi <93956895+habvi@users.noreply.github.com>
2024-10-11 16:27:30 +0900

    [add] integration multipart / post/400_23: fieldのfilenameに=がない場合を追加

--- test/webserv/integration/test_http_post.py
+++ test/webserv/integration/test_http_post.py
@@ -294,6 +294,11 @@ def test_post_201_responses(
             bad_request_response,
             MULTIPART_FILE_PATH1,
         ),
+        (
+            REQUEST_POST_4XX_DIR + "400_23_multipart_no_equal_in_field_filename.txt",
+            bad_request_response,
+            MULTIPART_FILE_PATH1,
+        ),
         # 403 is below -> test_post_403_responses()
         (
             REQUEST_POST_4XX_DIR + "404_01_non_exist_directory.txt",
@@ -369,6 +374,7 @@ def test_post_201_responses(
         "400_20_multipart_no_crlf_at_end",
         "400_21_multipart_no_crlf_at_end_of_body",
         "400_22_multipart_no_equal_in_field_name",
+        "400_23_multipart_no_equal_in_field_filename",
         "404_01_non_exist_directory",
         "405_01_method_not_allowed_for_uri",
         "408_01_shortened_body_message",
