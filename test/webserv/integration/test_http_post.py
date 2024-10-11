d345c0f5284861f1beeaa9a4a6c17dce8742f593
habvi <93956895+habvi@users.noreply.github.com>
2024-10-11 16:23:02 +0900

    [add] integration multipart / post/400_18: その他のheader fieldが2つ以上ある場合を追加

--- test/webserv/integration/test_http_post.py
+++ test/webserv/integration/test_http_post.py
@@ -268,6 +268,12 @@ def test_post_201_responses(
             bad_request_response,
             MULTIPART_FILE_PATH1,
         ),
+        (
+            REQUEST_POST_4XX_DIR
+            + "400_18_multipart_duplicate_header_field_in_same_part2.txt",
+            bad_request_response,
+            MULTIPART_FILE_PATH1,
+        ),
         # 403 is below -> test_post_403_responses()
         (
             REQUEST_POST_4XX_DIR + "404_01_non_exist_directory.txt",
@@ -338,6 +344,7 @@ def test_post_201_responses(
         "400_15_multipart_content_disposition_not_form_data",
         "400_16_multipart_no_name_in_disposition",
         "400_17_multipart_duplicate_header_field_in_same_part",
+        "400_18_multipart_duplicate_header_field_in_same_part2",
         "404_01_non_exist_directory",
         "405_01_method_not_allowed_for_uri",
         "408_01_shortened_body_message",
