40906003cd1ad6fe86fd2bed77dc6a357d41c35e
habvi <93956895+habvi@users.noreply.github.com>
2024-10-11 16:24:20 +0900

    [add] integration multipart / post/400_20: 最後にCRLFがない場合を追加

--- test/webserv/integration/test_http_post.py
+++ test/webserv/integration/test_http_post.py
@@ -279,6 +279,11 @@ def test_post_201_responses(
             bad_request_response,
             MULTIPART_FILE_PATH1,
         ),
+        (
+            REQUEST_POST_4XX_DIR + "400_20_multipart_no_crlf_at_end.txt",
+            bad_request_response,
+            MULTIPART_FILE_PATH1,
+        ),
         # 403 is below -> test_post_403_responses()
         (
             REQUEST_POST_4XX_DIR + "404_01_non_exist_directory.txt",
@@ -351,6 +356,7 @@ def test_post_201_responses(
         "400_17_multipart_duplicate_header_field_in_same_part",
         "400_18_multipart_duplicate_header_field_in_same_part2",
         "400_19_multipart_no_2_crlf_after_header",
+        "400_20_multipart_no_crlf_at_end",
         "404_01_non_exist_directory",
         "405_01_method_not_allowed_for_uri",
         "408_01_shortened_body_message",
